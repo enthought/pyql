852e1d3d48726cd7f5efdca332fede985ef47835
Didrik Pinte <dpinte@enthought.com>
2011-09-13 19:43:36 +0200

    cython issue: merging code of the methods

--- quantlib/quantlib/test/test_cython_bug.pyx
+++ quantlib/quantlib/test/test_cython_bug.pyx
@@ -4,7 +4,9 @@ from cython.operator cimport dereference as deref
 from libcpp.vector cimport vector
 
 from quantlib.instruments._bonds cimport FixedRateBond
-from quantlib.time._date cimport Date as QlDate, Date_todaysDate, Jul, August
+from quantlib.time._date cimport (
+    Date as QlDate, Date_todaysDate, Jul, August, September
+)
 from quantlib.time._period cimport Years, Period, Annual, Days
 from quantlib.time._calendar cimport (
     Calendar, TARGET, Unadjusted, ModifiedFollowing, Following
@@ -19,9 +21,24 @@ cdef extern from "quantlib/settings/ql_settings.hpp" namespace "QL":
 
 
 def test_bond_schedule_today_cython():
-    
     cdef QlDate today = Date_todaysDate()
-    set_evaluation_date(today)
+    cdef Calendar calendar = TARGET()
+	
+    cdef FixedRateBond* bond = get_bond_for_evaluation_date(today)
+
+    cdef QlDate s_date = calendar.advance(today, <Integer>3, Days, Following,
+            False)
+    cdef QlDate b_date = bond.settlementDate()
+
+    cdef Date s1 = date_from_qldate_ref(s_date)
+    cdef Date s2 = date_from_qldate_ref(b_date)
+
+    return s1, s2
+
+
+cdef FixedRateBond* get_bond_for_evaluation_date(QlDate& in_date):
+    
+    set_evaluation_date(in_date)
 
     cdef Calendar calendar = TARGET()
     cdef QlDate effective_date = QlDate(10, Jul, 2006)
@@ -62,67 +79,27 @@ def test_bond_schedule_today_cython():
             issue_date
     )
 
-    cdef QlDate s_date = calendar.advance(today, <Integer>3, Days, Following,
-            False)
-    cdef QlDate b_date = bond.settlementDate()
-
-    cdef Date s1 = date_from_qldate_ref(s_date)
-    cdef Date s2 = date_from_qldate_ref(b_date)
-
-    return s1, s2
+    return bond
 
 def test_bond_schedule_anotherday_cython():
     
     cdef QlDate today = QlDate(30, August, 2011)
-    set_evaluation_date(today)
+
+    cdef FixedRateBond* bond = get_bond_for_evaluation_date(today)
 
     cdef Calendar calendar = TARGET()
-    cdef QlDate effective_date = QlDate(10, Jul, 2006)
-
-    cdef QlDate termination_date = calendar.advance(
-        effective_date, <Integer>10, Years, Unadjusted, False
-    )
-
-    cdef Natural settlement_days = 3
-    cdef Real face_amount = 100.0
-    cdef Real coupon_rate = 0.05
-    cdef Real redemption = 100.0
-
-    cdef Schedule fixed_bond_schedule = Schedule(
-            effective_date,
-            termination_date,
-            Period(Annual),
-            calendar,
-            ModifiedFollowing,
-            ModifiedFollowing,
-            Backward,
-            False
-    )
-
-    cdef QlDate issue_date = QlDate(10, Jul, 2006)
-
-    cdef vector[Rate]* coupons = new vector[Rate]()
-    coupons.push_back(coupon_rate)
-
-    cdef FixedRateBond* bond = new FixedRateBond(
-            settlement_days,
-	    face_amount,
-	    fixed_bond_schedule,
-	    deref(coupons),
-            ActualActual(ISMA), 
-	    Following,
-            redemption,
-            issue_date
-    )
-
     cdef QlDate s_date = calendar.advance(today, <Integer>3, Days, Following,
             False)
     cdef QlDate b_date = bond.settlementDate()
+
+    cdef QlDate e_date = get_evaluation_date()
 	
     print s_date.serialNumber()
     print b_date.serialNumber()
 
     cdef Date s1 = date_from_qldate_ref(s_date)
     cdef Date s2 = date_from_qldate_ref(b_date)
+    cdef Date s3 = date_from_qldate_ref(e_date)
+    print s3
 
     return s1, s2
