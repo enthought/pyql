200915069779d34f186756242c5795bca0a413cc
Didrik Pinte <dpinte@enthought.com>
2011-09-17 22:00:00 +0200

    moving the test related to the Settings issue to test_settings
    - moved tests from test_bonds to test_settings
    - updates the time api file

--- quantlib/quantlib/time/api.py
+++ quantlib/quantlib/time/api.py
@@ -1,7 +1,12 @@
-from .calendar import Calendar, TARGET
+from .calendar import (
+    Calendar, TARGET, ModifiedFollowing, Following, Unadjusted
+)
 from .calendars.null_calendar import NullCalendar
 from .daycounter import Actual360, Actual365Fixed
+from .daycounters.actual_actual import ActualActual, ISMA
 from .date import (
-    Date, January, February, March, April, May, Months, Period, today, Years
+    Date, January, February, March, April, May, Months, Period, today, Years,
+    Days, August, Jul, Annual
 )
+from .schedule import Schedule, Backward
 
