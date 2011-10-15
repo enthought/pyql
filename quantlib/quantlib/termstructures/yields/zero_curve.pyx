1b4125984cfe55309bb23a7e4a22815fea6cbd2b
Didrik Pinte <dpinte@enthought.com>
2011-10-15 15:04:03 +0200

    adding ZeroCurve support

---
+++ quantlib/quantlib/termstructures/yields/zero_curve.pyx
@@ -0,0 +1,40 @@
+include '../../types.pxi'
+
+from cython.operator cimport dereference as deref
+from libcpp.vector cimport vector
+
+cimport _zero_curve as _zc
+from quantlib.handle cimport shared_ptr
+from flat_forward cimport YieldTermStructure
+from quantlib.time.daycounter cimport DayCounter
+from quantlib.time.date cimport Date
+
+cdef class ZeroCurve(YieldTermStructure):
+
+    def __cinit__(self):
+        pass
+
+    def __dealloc__(self):
+        pass
+
+    def __init__(self, dates, yields, DayCounter daycounter):
+
+        # convert dates and yields to vector
+        cdef vector[_zc.Date]* _date_vector = new vector[_zc.Date]()
+        cdef vector[Rate]* _yield_vector = new vector[Rate]()
+
+        # highly inefficient and could be improved
+        for date in dates:
+            _date_vector.push_back(deref((<Date>date)._thisptr))
+
+        for rate in yields:
+            _yield_vector.push_back(rate)
+
+        # create the curve
+        self._thisptr = new shared_ptr[_zc.YieldTermStructure](
+            new _zc.ZeroCurve(
+                deref(_date_vector),
+                deref(_yield_vector),
+                deref(daycounter._thisptr)
+            )
+        )
