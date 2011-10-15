f6c3f4fb540077632e755232a9cd8679be1b9241
Didrik Pinte <dpinte@enthought.com>
2011-10-15 15:04:49 +0200

    exposing some missing import in the time/api.py file

--- quantlib/quantlib/time/api.py
+++ quantlib/quantlib/time/api.py
@@ -6,7 +6,7 @@ from .daycounter import Actual360, Actual365Fixed
 from .daycounters.actual_actual import ActualActual, ISMA
 from .date import (
     Date, January, February, March, April, May, Months, Period, today, Years,
-    Days, August, Jul, Annual
+    Days, August, Jul, Annual, July, Weeks
 )
 from .schedule import Schedule, Backward
 
