21f54e5c7138a007b1abd8da8e119e9c02c2d2de
Didrik Pinte <dpinte@enthought.com>
2011-11-19 16:57:39 +0100

    support for the term_structure_factory in progress

--- quantlib/quantlib/termstructures/yields/piecewise_yield_curve.pyx
+++ quantlib/quantlib/termstructures/yields/piecewise_yield_curve.pyx
@@ -13,29 +13,48 @@ from rate_helpers cimport RateHelper
 from quantlib.time.date cimport Date
 from quantlib.time.daycounter cimport DayCounter
 
+from quantlib.termstructures.yields.yield_term_structure cimport YieldTermStructure
+
 # Plan
 # - implemente the RateHelper
 # - do not expose the piecewise yield curve but only the termstructure
 #   allowing to get down to the curve if needed.
 
+VALID_TRAITS = ['discount', 'forward', 'zero']
 
 def term_structure_factory(str traits, str interpolator, Date settlement_date,
     rate_helpers, DayCounter day_counter, Real tolerance):
 
-    # convert rate_helpers list to std::vetor
+    # validate traits
+    if traits not in VALID_TRAITS:
+        raise ValueError('Traits must be in {}',format(VALID_TRAITS))
 
+    # convert rate_helpers list to std::vetor
     cdef vector[shared_ptr[RateHelper]]* curve_inputs = new vector[shared_ptr[RateHelper]]()
     for helper in rate_helpers:
         curve_inputs.push_back( deref((<RateHelper>helper)._thisptr))
 
-    cdef shared_ptr[_ff.YieldTermStructure]* ts = _pyc.term_structure_factory(
-        string(PyString_AsString(traits)),
-        string(PyString_AsString(interpolator)),
-        deref(settlement_date._thisptr),
+    # convert the Python str to C++ string
+    cdef string* traits_string = new string(PyString_AsString(traits))
+    cdef string* interpolator_string = new string(PyString_AsString(interpolator)),
+
+    cdef shared_ptr[_ff.YieldTermStructure] ts_ptr = _pyc.term_structure_factory(
+        deref(traits_string),
+        deref(interpolator_string),
+        deref(settlement_date._thisptr.get()),
         deref(curve_inputs),
         deref(day_counter._thisptr),
         tolerance
     )
+
+    term_structure = YieldTermStructure()
+    del term_structure._thisptr
+    term_structure._thisptr = new shared_ptr[_ff.YieldTermStructure](ts_ptr.get())
+    return term_structure
+
+
+
+
 
 
 
