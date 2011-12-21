4c0ba68829711b116db682670e5f675557267714
Didrik Pinte <dpinte@enthought.com>
2011-12-21 09:07:39 +0100

    examples: pyflaking settings_test.py

--- quantlib/examples/settings_test.py
+++ quantlib/examples/settings_test.py
@@ -1,38 +1,35 @@
 # simple example to demonstrate the use of Settings()
 
-from quantlib.termstructures.yields.flat_forward import (
-    SimpleQuote, FlatForward, YieldTermStructure
-)
 
+from quantlib.quotes import SimpleQuote
 from quantlib.settings import Settings
-from quantlib.time.calendar import TARGET
-from quantlib.time.calendars.null_calendar import NullCalendar
-from quantlib.time.daycounter import Actual360, Actual365Fixed
-from quantlib.time.date import today, Days, Date
+from quantlib.termstructures.yields.api import FlatForward
+from quantlib.time.api import Actual360, Date, NullCalendar, TARGET
 
 calendar = TARGET()
+settings = Settings()
 
-dt_today = Date(6,9,2011)
-dt_payment = Date(6,10,2011)
+date_today      = Date(6,9,2011)
+date_payment    = Date(6,10,2011)
 settlement_days = 2
 
-Settings().evaluation_date = dt_today 
-quote = SimpleQuote()
-quote.value = 0.03
+settings.evaluation_date = date_today
+quote = SimpleQuote(value=0.03)
 
 term_structure = FlatForward(
-    settlement_days = settlement_days, 
-    quote           = quote, 
-    calendar        = NullCalendar(), 
+    settlement_days = settlement_days,
+    quote           = quote,
+    calendar        = NullCalendar(),
     daycounter      = Actual360()
 )
 
-df_1 = term_structure.discount(dt_payment)
+df_1 = term_structure.discount(date_payment)
 
-dt_today = Date(19,9,2011)
-Settings().evaluation_date = dt_today 
-dt_payment = Date(19,10,2011)
-df_2 = term_structure.discount(dt_payment)
+date_today = Date(19,9,2011)
+settings.evaluation_date = date_today
+
+date_payment = Date(19,10,2011)
+df_2 = term_structure.discount(date_payment)
 
 # df_1 and df_2 should be identical:
 print('rate: %f df_1: %f df_2 %f difference: %f' % (quote.value, df_1, df_2, df_2-df_1))
@@ -41,5 +38,5 @@ print('rate: %f df_1: %f df_2 %f difference: %f' % (quote.value, df_1, df_2, df_
 # triggers a lazy recalculation of the discount factor
 
 quote.value = .05
-df_2 = term_structure.discount(dt_payment)
+df_2 = term_structure.discount(date_payment)
 print('rate: %f df_2: %f' % (quote.value, df_2))
