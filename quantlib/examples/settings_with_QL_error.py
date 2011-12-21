c56ef40ea559eeea6a685e7feee66226ab4e3b0e
Didrik Pinte <dpinte@enthought.com>
2011-12-21 09:03:32 +0100

    examples: pyflaking settings_with_QL_error.py

--- quantlib/examples/settings_with_QL_error.py
+++ quantlib/examples/settings_with_QL_error.py
@@ -1,34 +1,36 @@
 # This code throws a QuantLib::Error that terminates python
 # Settings is set by default to today's date.
 # If dt_payment is in the past, a QuantLib::Error is thrown from c++
+import logging
 
-from quantlib.termstructures.yields.flat_forward import (
-    FlatForward, YieldTermStructure
-)
-from quantlib.quotes.simplequote import SimpleQuote
 
-from quantlib.settings import Settings
-from quantlib.time.calendar import TARGET
-from quantlib.time.calendars.null_calendar import NullCalendar
-from quantlib.time.daycounter import Actual360, Actual365Fixed
-from quantlib.time.date import today, Days, Date
+from quantlib.quotes import SimpleQuote
+from quantlib.termstructures.yields.api import FlatForward
+from quantlib.time.api import Actual360, Date, NullCalendar, TARGET
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 calendar = TARGET()
 
-dt_today = Date(6,9,2011)
-dt_payment = Date(6,12,2000)
+date_today      = Date(6,9,2011)
+date_payment    = Date(6,12,2000)
 settlement_days = 2
 
-quote = SimpleQuote()
-quote.value = 0.03
+quote = SimpleQuote(value=0.03)
 
 term_structure = FlatForward(
-    settlement_days = settlement_days, 
-    quote           = quote, 
-    calendar        = NullCalendar(), 
+    settlement_days = settlement_days,
+    quote           = quote,
+    calendar        = NullCalendar(),
     daycounter      = Actual360()
 )
 
-df_1 = term_structure.discount(dt_payment)
-print('rate: %f df_1: %f' % (quote.value, df_1))
+try:
+    df_1 = term_structure.discount(date_payment)
+    print('rate: %f df_1: %f' % (quote.value, df_1))
+except RuntimeError as exc:
+    logger.error('Evaluation date and discount date issue.')
+    logger.exception(exc)
 
