9080c31c5de7b975fb29d80bd67999fe4731755d
Didrik Pinte <dpinte@enthought.com>
2012-06-12 19:52:56 +0200

    WIP on the cds.py example

--- examples/cds.py
+++ examples/cds.py
@@ -3,7 +3,12 @@
 """
 
 from quantlib.quotes import SimpleQuote
-from quantlib.time.api import Calendar, Date, May, Actual365Fixed, Following
+from quantlib.settings import Settings
+from quantlib.time.api import (
+    Calendar, Date, May, Actual365Fixed, Following, TARGET, Period, Months,
+    Quarterly, TwentiethIMM, Years
+)
+from quantlib.termstructures.credit.api import SpreadCdsHelper, PiecewiseDefaultCurve
 from quantlib.termstructures.yields.api import FlatForward
 
 if __name__ == '__main__':
@@ -20,9 +25,8 @@ if __name__ == '__main__':
     Settings.instance().evaluation_date = todays_date
 
     # dummy curve
-    flat_rate = 0.01
     ts_curve = FlatForward(
-        reference_date=todays_date, quote=flat_rate, daycounter=Actual365Fixed()
+        reference_date=todays_date, forward=0.01, daycounter=Actual365Fixed()
     )
 
     # In Lehmans Brothers "guide to exotic credit derivatives"
@@ -45,150 +49,137 @@ if __name__ == '__main__':
             Following, TwentiethIMM, Actual365Fixed(), recovery_rate, ts_curve
         )
 
-        // Bootstrap hazard rates
-        boost::shared_ptr<PiecewiseDefaultCurve<HazardRate, BackwardFlat> >
-           hazardRateStructure(
-               new PiecewiseDefaultCurve<HazardRate, BackwardFlat>(
-                                                           todaysDate,
-                                                           instruments,
-                                                           Actual365Fixed()));
-        vector<pair<Date, Real> > hr_curve_data = hazardRateStructure->nodes();
+        instruments.append(helper)
 
-        cout << "Calibrated hazard rate values: " << endl ;
-        for (Size i=0; i<hr_curve_data.size(); i++) {
-            cout << "hazard rate on " << hr_curve_data[i].first << " is "
-                 << hr_curve_data[i].second << endl;
-        }
-        cout << endl;
+    # Bootstrap hazard rates
+    hazard_rate_structure = PiecewiseDefaultCurve(
+        'HazardRate', 'BackwardFlat', todays_date, instruments, Actual365Fixed()
+    )
 
-        cout << "Some survival probability values: " << endl ;
-        cout << "1Y survival probability: "
-             << io::percent(hazardRateStructure->survivalProbability(
-                                                        todaysDate + 1*Years))
-             << endl
-             << "               expected: "
-             << io::percent(0.9704)
-             << endl;
-        cout << "2Y survival probability: "
-             << io::percent(hazardRateStructure->survivalProbability(
-                                                        todaysDate + 2*Years))
-             << endl
-             << "               expected: "
-             << io::percent(0.9418)
-             << endl;
+    #vector<pair<Date, Real> > hr_curve_data = hazardRateStructure->nodes();
 
-        cout << endl << endl;
+    #cout << "Calibrated hazard rate values: " << endl ;
+    #for (Size i=0; i<hr_curve_data.size(); i++) {
+    #    cout << "hazard rate on " << hr_curve_data[i].first << " is "
+    #         << hr_curve_data[i].second << endl;
+    #}
+    #cout << endl;
 
-        // reprice instruments
-        Real nominal = 1000000.0;
-        Handle<DefaultProbabilityTermStructure> probability(hazardRateStructure);
-        boost::shared_ptr<PricingEngine> engine(
-                  new MidPointCdsEngine(probability, recovery_rate, tsCurve));
 
-        Schedule cdsSchedule =
-            MakeSchedule().from(todaysDate).to(maturities[0])
-                          .withFrequency(Quarterly)
-                          .withCalendar(calendar)
-                          .withTerminationDateConvention(Unadjusted)
-                          .withRule(DateGeneration::TwentiethIMM);
-        CreditDefaultSwap cds_3m(Protection::Seller,
-                                 nominal,
-                                 quoted_spreads[0],
-                                 cdsSchedule,
-                                 Following,
-                                 Actual365Fixed());
+    target = todays_date + Period(1, Years)
+    print target
+    print "Some survival probability values: "
+    print "1Y survival probability: {:%}".format(
+            hazard_rate_structure.survival_probability(target)
+    )
+    print "               expected: {:%}".format(0.9704)
 
-        cdsSchedule =
-            MakeSchedule().from(todaysDate).to(maturities[1])
-                          .withFrequency(Quarterly)
-                          .withCalendar(calendar)
-                          .withTerminationDateConvention(Unadjusted)
-                          .withRule(DateGeneration::TwentiethIMM);
-        CreditDefaultSwap cds_6m(Protection::Seller,
-                                 nominal,
-                                 quoted_spreads[1],
-                                 cdsSchedule,
-                                 Following,
-                                 Actual365Fixed());
+    print "2Y survival probability: {:%}".format(
+        hazard_rate_structure.survival_probability(todays_date + 2*Years)
+    )
+    print "               expected: {:%}".format(0.9418)
 
-        cdsSchedule =
-            MakeSchedule().from(todaysDate).to(maturities[2])
-                          .withFrequency(Quarterly)
-                          .withCalendar(calendar)
-                          .withTerminationDateConvention(Unadjusted)
-                          .withRule(DateGeneration::TwentiethIMM);
-        CreditDefaultSwap cds_1y(Protection::Seller,
-                                 nominal,
-                                 quoted_spreads[2],
-                                 cdsSchedule,
-                                 Following,
-                                 Actual365Fixed());
+    # reprice instruments
+    nominal = 1000000.0;
+    #Handle<DefaultProbabilityTermStructure> probability(hazardRateStructure);
+    engine = MidPointCdsEngine(hazard_rate_structure, recovery_rate, ts_curve)
 
-        cdsSchedule =
-            MakeSchedule().from(todaysDate).to(maturities[3])
-                          .withFrequency(Quarterly)
-                          .withCalendar(calendar)
-                          .withTerminationDateConvention(Unadjusted)
-                          .withRule(DateGeneration::TwentiethIMM);
-        CreditDefaultSwap cds_2y(Protection::Seller,
-                                 nominal,
-                                 quoted_spreads[3],
-                                 cdsSchedule,
-                                 Following,
-                                 Actual365Fixed());
+    #Schedule cdsSchedule =
+    #    MakeSchedule().from(todaysDate).to(maturities[0])
+    #                  .withFrequency(Quarterly)
+    #                  .withCalendar(calendar)
+    #                  .withTerminationDateConvention(Unadjusted)
+    #                  .withRule(DateGeneration::TwentiethIMM);
+    #CreditDefaultSwap cds_3m(Protection::Seller,
+    #                         nominal,
+    #                         quoted_spreads[0],
+    #                         cdsSchedule,
+    #                         Following,
+    #                         Actual365Fixed());
 
-        cds_3m.setPricingEngine(engine);
-        cds_6m.setPricingEngine(engine);
-        cds_1y.setPricingEngine(engine);
-        cds_2y.setPricingEngine(engine);
+    #cdsSchedule =
+    #    MakeSchedule().from(todaysDate).to(maturities[1])
+    #                  .withFrequency(Quarterly)
+    #                  .withCalendar(calendar)
+    #                  .withTerminationDateConvention(Unadjusted)
+    #                  .withRule(DateGeneration::TwentiethIMM);
+    #CreditDefaultSwap cds_6m(Protection::Seller,
+    #                         nominal,
+    #                         quoted_spreads[1],
+    #                         cdsSchedule,
+    #                         Following,
+    #                         Actual365Fixed());
 
-        cout << "Repricing of quoted CDSs employed for calibration: " << endl;
-        cout << "3M fair spread: " << io::rate(cds_3m.fairSpread()) << endl
-             << "   NPV:         " << cds_3m.NPV() << endl
-             << "   default leg: " << cds_3m.defaultLegNPV() << endl
-             << "   coupon leg:  " << cds_3m.couponLegNPV() << endl
-             << endl;
+    #cdsSchedule =
+    #    MakeSchedule().from(todaysDate).to(maturities[2])
+    #                  .withFrequency(Quarterly)
+    #                  .withCalendar(calendar)
+    #                  .withTerminationDateConvention(Unadjusted)
+    #                  .withRule(DateGeneration::TwentiethIMM);
+    #CreditDefaultSwap cds_1y(Protection::Seller,
+    #                         nominal,
+    #                         quoted_spreads[2],
+    #                         cdsSchedule,
+    #                         Following,
+    #                         Actual365Fixed());
 
-        cout << "6M fair spread: " << io::rate(cds_6m.fairSpread()) << endl
-             << "   NPV:         " << cds_6m.NPV() << endl
-             << "   default leg: " << cds_6m.defaultLegNPV() << endl
-             << "   coupon leg:  " << cds_6m.couponLegNPV() << endl
-             << endl;
+    #cdsSchedule =
+    #    MakeSchedule().from(todaysDate).to(maturities[3])
+    #                  .withFrequency(Quarterly)
+    #                  .withCalendar(calendar)
+    #                  .withTerminationDateConvention(Unadjusted)
+    #                  .withRule(DateGeneration::TwentiethIMM);
+    #CreditDefaultSwap cds_2y(Protection::Seller,
+    #                         nominal,
+    #                         quoted_spreads[3],
+    #                         cdsSchedule,
+    #                         Following,
+    #                         Actual365Fixed());
 
-        cout << "1Y fair spread: " << io::rate(cds_1y.fairSpread()) << endl
-             << "   NPV:         " << cds_1y.NPV() << endl
-             << "   default leg: " << cds_1y.defaultLegNPV() << endl
-             << "   coupon leg:  " << cds_1y.couponLegNPV() << endl
-             << endl;
+    #cds_3m.setPricingEngine(engine);
+    #cds_6m.setPricingEngine(engine);
+    #cds_1y.setPricingEngine(engine);
+    #cds_2y.setPricingEngine(engine);
 
-        cout << "2Y fair spread: " << io::rate(cds_2y.fairSpread()) << endl
-             << "   NPV:         " << cds_2y.NPV() << endl
-             << "   default leg: " << cds_2y.defaultLegNPV() << endl
-             << "   coupon leg:  " << cds_2y.couponLegNPV() << endl
-             << endl;
+    #cout << "Repricing of quoted CDSs employed for calibration: " << endl;
+    #cout << "3M fair spread: " << io::rate(cds_3m.fairSpread()) << endl
+    #     << "   NPV:         " << cds_3m.NPV() << endl
+    #     << "   default leg: " << cds_3m.defaultLegNPV() << endl
+    #     << "   coupon leg:  " << cds_3m.couponLegNPV() << endl
+    #     << endl;
 
-        cout << endl << endl;
+    #cout << "6M fair spread: " << io::rate(cds_6m.fairSpread()) << endl
+    #     << "   NPV:         " << cds_6m.NPV() << endl
+    #     << "   default leg: " << cds_6m.defaultLegNPV() << endl
+    #     << "   coupon leg:  " << cds_6m.couponLegNPV() << endl
+    #     << endl;
 
-        Real seconds  = timer.elapsed();
-        Integer hours = Integer(seconds/3600);
-        seconds -= hours * 3600;
-        Integer minutes = Integer(seconds/60);
-        seconds -= minutes * 60;
-        cout << "Run completed in ";
-        if (hours > 0)
-            cout << hours << " h ";
-        if (hours > 0 || minutes > 0)
-            cout << minutes << " m ";
-        cout << fixed << setprecision(0)
-             << seconds << " s" << endl;
+    #cout << "1Y fair spread: " << io::rate(cds_1y.fairSpread()) << endl
+    #     << "   NPV:         " << cds_1y.NPV() << endl
+    #     << "   default leg: " << cds_1y.defaultLegNPV() << endl
+    #     << "   coupon leg:  " << cds_1y.couponLegNPV() << endl
+    #     << endl;
 
-        return 0;
-    } catch (exception& e) {
-        cerr << e.what() << endl;
-        return 1;
-    } catch (...) {
-        cerr << "unknown error" << endl;
-        return 1;
-    }
-}
+    #cout << "2Y fair spread: " << io::rate(cds_2y.fairSpread()) << endl
+    #     << "   NPV:         " << cds_2y.NPV() << endl
+    #     << "   default leg: " << cds_2y.defaultLegNPV() << endl
+    #     << "   coupon leg:  " << cds_2y.couponLegNPV() << endl
+    #     << endl;
+
+    #cout << endl << endl;
+
+    #Real seconds  = timer.elapsed();
+    #Integer hours = Integer(seconds/3600);
+    #seconds -= hours * 3600;
+    #Integer minutes = Integer(seconds/60);
+    #seconds -= minutes * 60;
+    #cout << "Run completed in ";
+    #if (hours > 0)
+    #    cout << hours << " h ";
+    #if (hours > 0 || minutes > 0)
+    #    cout << minutes << " m ";
+    #cout << fixed << setprecision(0)
+    #     << seconds << " s" << endl;
+
+    #return 0;
 
