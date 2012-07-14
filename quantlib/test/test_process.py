445ffa581bfaa9e1f892a5c04c100cf1ca1cbfa9
Didrik Pinte <dpinte@enthought.com>
2012-07-14 16:50:32 +0200

    updating test_process to do some testing ...

--- quantlib/test/test_process.py
+++ quantlib/test/test_process.py
@@ -1,9 +1,4 @@
-import numpy as np
-
-
-from quantlib.models.equity.heston_model import (
-    HestonModelHelper, HestonModel, ImpliedVolError
-)
+import unittest
 
 from quantlib.models.equity.bates_model import (BatesModel, BatesDoubleExpModel)
 from quantlib.processes.heston_process import HestonProcess
@@ -11,12 +6,11 @@ from quantlib.processes.bates_process import BatesProcess
 
 from quantlib.settings import Settings
 from quantlib.time.api import (
-    today, Actual360, NullCalendar, Period, Months, Years, Date, July,
-    Actual365Fixed, TARGET, Weeks, ActualActual
+    today, NullCalendar, ActualActual
 )
 from quantlib.termstructures.yields.flat_forward import FlatForward
 from quantlib.quotes import SimpleQuote
-from quantlib.termstructures.yields.zero_curve import ZeroCurve
+
 
 def flat_rate(forward, daycounter):
     return FlatForward(
@@ -26,55 +20,62 @@ def flat_rate(forward, daycounter):
         daycounter      = daycounter
     )
 
-DtSettlement = today()
 
-settings = Settings()
-settings.evaluation_date = DtSettlement
+class ProcessTestCase(unittest.TestCase):
 
-daycounter = ActualActual()
-calendar = NullCalendar()
+    def setUp(self):
+        settlement_date = today()
 
-iRate = .1
-iDiv = .04
+        settings = Settings()
+        settings.evaluation_date = settlement_date
 
-risk_free_ts = flat_rate(iRate, daycounter)
-dividend_ts = flat_rate(iDiv, daycounter)
+        daycounter = ActualActual()
+        self.calendar = NullCalendar()
 
-s0 = SimpleQuote(32.0)
+        i_rate = .1
+        i_div = .04
 
-# Bates model
+        self.risk_free_ts = flat_rate(i_rate, daycounter)
+        self.dividend_ts = flat_rate(i_div, daycounter)
 
-v0 = 0.05
-kappa = 5.0;
-theta = 0.05;
-sigma = 1.0e-4;
-rho = 0.0;
-Lambda = .1
-nu = .01
-delta = .001
+        self.s0 = SimpleQuote(32.0)
 
-pb = BatesProcess(risk_free_ts, dividend_ts, s0, v0,
-                       kappa, theta, sigma, rho,
-                       Lambda, nu, delta)
+        # Bates model
 
-print(pb)
+        self.v0 = 0.05
+        self.kappa = 5.0
+        self.theta = 0.05
+        self.sigma = 1.0e-4
+        self.rho = 0.0
+        self.Lambda = .1
+        self.nu = .01
+        self.delta = .001
 
-# error: the arguments Lambda, nu, delta have changed
-mb = BatesModel(pb)
-print(mb)
+    def test_batest_process(self):
+        pb = BatesProcess(self.risk_free_ts, self.dividend_ts, self.s0, self.v0,
+                          self.kappa, self.theta, self.sigma, self.rho,
+                          self.Lambda, self.nu, self.delta)
 
-# double exponential model
+        self.assertIsNotNone(pb)
 
-ph = HestonProcess(risk_free_ts, dividend_ts, s0, v0,
-                       kappa, theta, sigma, rho)
+        mb = BatesModel(pb)
 
-print(ph)
+        self.assertIsNotNone(mb)
 
-# constructor with default arguments: it works
-me = BatesDoubleExpModel(ph)
-print(me)
+    def test_heston_process(self):
 
-# speficy the arguments: it works
-me = BatesDoubleExpModel(ph, Lambda=0.234, nuUp=0.43, nuDown=0.54, p=.6)
-print(me)
+        ph = HestonProcess(self.risk_free_ts, self.dividend_ts, self.s0, self.v0,
+                            self.kappa, self.theta, self.sigma, self.rho)
+        self.assertIsNotNone(ph)
+
+        # constructor with default arguments
+        me = BatesDoubleExpModel(ph)
+        self.assertIsNotNone(me)
+
+        # speficy the arguments
+        me = BatesDoubleExpModel(ph, Lambda=0.234, nuUp=0.43, nuDown=0.54, p=.6)
+        self.assertIsNotNone(me)
+
+if __name__ == '__main__':
+    unittest.main()
 
