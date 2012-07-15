e0acfa0712d01ba14b126cd63ec54a92888c666d
Didrik Pinte <dpinte@enthought.com>
2012-07-15 21:29:01 +0200

    refactored to match pep8 and typical Python conventions

--- quantlib/util/converter.py
+++ quantlib/util/converter.py
@@ -1,26 +1,21 @@
-import pandas
-from pandas import DataFrame
-import datetime
 
-from quantlib.time.api import Period, Date, Actual365Fixed, TARGET, Days
+from quantlib.time.api import Date, Actual365Fixed
 from quantlib.termstructures.yields.zero_curve import ZeroCurve
 
-def dateToQLDate(dt):
-    """
-    Converts a datetime object into a QL Date
-    """
-    
-    return Date(dt.day, dt.month, dt.year)
+def pydate_to_qldate(date):
+    """ Converts a datetime object into a QL Date. """
 
-def dfToZeroCurve(df_rates, dtSettlement, daycounter=Actual365Fixed()):
-    """
-    Convert a panda data frame into a QL zero curve
-    """
-    
-    dates = [dateToQLDate(dt) for dt in df_rates.index]
-    dates.insert(0, dateToQLDate(dtSettlement))
-    dates.append(dates[-1]+365*2)
-    vx = list(df_rates.values)
-    vx.insert(0, vx[0])
-    vx.append(vx[-1])
-    return ZeroCurve(dates, vx, daycounter)
+    return Date(date.day, date.month, date.year)
+
+def df_to_zero_curve(rates, settlement_date, daycounter=Actual365Fixed()):
+    """ Converts a pandas data frame into a QL zero curve. """
+
+    dates = [pydate_to_qldate(dt) for dt in rates.index]
+    dates.insert(0, pydate_to_qldate(settlement_date))
+    dates.append(dates[-1] + 365 * 2) # why adding two years here. Is it conventional?
+
+    values = rates.values.tolist()
+    values.insert(0, values[0])
+    values.append(values[-1])
+
+    return ZeroCurve(dates, values, daycounter)
