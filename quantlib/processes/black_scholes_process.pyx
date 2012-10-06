ec95bfa004325ddf5c8c766a5a9208975cafca85
P. Henaff <henaff.iae@univ-paris1.fr>
2012-10-07 01:26:05 +0200

    work in progress: new signature in date converter

--- quantlib/processes/black_scholes_process.pyx
+++ quantlib/processes/black_scholes_process.pyx
@@ -54,3 +54,8 @@ cdef class BlackScholesMertonProcess(GeneralizedBlackScholesProcess):
                 black_vol_ts_handle
             )
         )
+
+        property x0:
+            def __get__(self):
+                return self._thisptr.get().x0()
+            
