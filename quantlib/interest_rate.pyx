5107a32ccc7bb069ed66e82d87d9a149a58a7b3a
Didrik Pinte <dpinte@enthought.com>
2012-12-13 14:33:39 +0100

    exposes the InterestRate class in pyql

---
+++ quantlib/interest_rate.pyx
@@ -0,0 +1,74 @@
+"""
+ Copyright (C) 2011, Enthought Inc
+ Copyright (C) 2011, Patrick Henaff
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+"""
+
+include 'types.pxi'
+
+from cython.operator cimport dereference as deref
+
+from quantlib.handle cimport shared_ptr
+cimport _interest_rate as _ir
+
+from quantlib.time.daycounter cimport DayCounter
+
+cdef class InterestRate:
+    """ This class encapsulate the interest rate compounding algebra.
+
+    It manages day-counting conventions, compounding conventions,
+    conversion between different conventions, discount/compound factor
+    calculations, and implied/equivalent rate calculations.
+
+    """
+
+
+    def __dealloc__(self):
+        if self._thisptr is not NULL:
+            del self._thisptr
+            self._thisptr = NULL
+
+    def __cinit__(self, float rate, DayCounter dc, int compounding, int frequency, **kwargs):
+
+        if 'noalloc' in kwargs:
+            return
+        self._thisptr = new shared_ptr[_ir.InterestRate](
+            new _ir.InterestRate(
+                <Rate>rate, deref(dc._thisptr), <_ir.Compounding>compounding,
+                <_ir.Frequency>frequency
+            )
+        )
+
+
+    property rate:
+        def __get__(self):
+            return self._thisptr.get().rate()
+
+    property compounding:
+        def __get__(self):
+            return self._thisptr.get().compounding()
+
+    property frequency:
+        """ Returns the frequency used for computation.
+
+        If the Frequency does not make sense, it returns NoFrequency.
+        Frequency only make sense when compounding is Compounded or
+        SingleThenCompounded.
+
+        """
+        def __get__(self):
+            return self._thisptr.get().frequency()
+
+
+    property day_counter:
+        def __get__(self):
+            cdef _ir.DayCounter dc = self._thisptr.get().dayCounter()
+
+            counter = DayCounter()
+            counter._thisptr = &dc_ptr
+
+            return counter
+
---
+++ quantlib/test/test_interest_rate.py
@@ -0,0 +1,58 @@
+import unittest
+
+from quantlib.interest_rate import InterestRate
+
+from quantlib.compounding import Continuous, Compounded
+from quantlib.time.api import Actual360, Monthly, NoFrequency, Once
+
+class InterestRateTestCase(unittest.TestCase):
+
+    def setUp(self):
+
+        self.day_counter = Actual360()
+
+    def test_create_interest_rate_frequency_makes_no_sense(self):
+
+
+        rate = 0.05
+        counter = self.day_counter
+        compounding = Continuous
+        frequency = Monthly
+
+        interest_rate = InterestRate(rate, counter, compounding, frequency)
+
+        self.assertAlmostEquals(interest_rate.rate, rate)
+        self.assertEquals(interest_rate.compounding, compounding)
+
+        # Returns NoFrequency when it does not make sense
+        self.assertEquals(interest_rate.frequency, NoFrequency)
+
+        # Broken check. DayCoonter != Actual360
+        self.assertEquals(interest_rate.day_counter, Actual360())
+
+
+    def test_create_interest_rate_compounded(self):
+
+        rate = 0.05
+        counter = self.day_counter
+        compounding = Compounded
+        frequency = Monthly
+
+        interest_rate = InterestRate(rate, counter, compounding, frequency)
+
+        self.assertAlmostEquals(interest_rate.rate, rate)
+        self.assertEquals(interest_rate.compounding, compounding)
+        self.assertEquals(interest_rate.frequency, Monthly)
+
+    def test_create_interest_rate_compounded_error(self):
+
+        rate = 0.05
+        counter = self.day_counter
+        compounding = Compounded
+
+        for frequency in [Once, NoFrequency]:
+            with self.assertRaises(RuntimeError):
+                InterestRate(rate, counter, compounding, frequency)
+
+if __name__ == '__main__':
+    unittest.main()
