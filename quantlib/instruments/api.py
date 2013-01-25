01ba8e3b144c5493d04678c9f5478ffb0258a9ad
Didrik Pinte <dpinte@enthought.com>
2013-01-25 22:20:30 +0100

    updated api module

--- quantlib/instruments/api.py
+++ quantlib/instruments/api.py
@@ -1,5 +1,6 @@
 
 from .bonds import FixedRateBond, ZeroCouponBond
 from .credit_default_swap import CreditDefaultSwap
-from .option import EuropeanExercise, AmericanExercise, VanillaOption, DividendVanillaOption
-from .payoffs import Put, Call, PlainVanillaPayoffs, PAYOFF_TO_STR
+from .option import EuropeanExercise, AmericanExercise, VanillaOption
+from .option import DividendVanillaOption, EuropeanOption
+from .payoffs import Put, Call, PlainVanillaPayoff, PAYOFF_TO_STR
