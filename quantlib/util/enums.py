c3d117073349698d97927c77908768f915eab816
phenaff <henaff.iae@univ-paris1.fr>
2014-01-03 01:35:16 -0500

    PEP8 clean up

--- quantlib/util/enums.py
+++ quantlib/util/enums.py
@@ -23,15 +23,15 @@ class TimeUnits(object):
 
 class Compounding(object):
     Simple = 0     # 1+rt
-    Compounded = 1 # (1+r)^t
-    Continuous = 2 # e^{rt}
-    SimpleThenCompounded = 3 # Simple up to the first period then Compounded
+    Compounded = 1  # (1+r)^t
+    Continuous = 2  # e^{rt}
+    SimpleThenCompounded = 3  # Simple up to the first period then Compounded
 
 
 class BusinessDayConventions(object):
     from quantlib.time.calendar import (
         ModifiedFollowing, Following, ModifiedPreceding,
-        Preceding, Unadjusted, 
+        Preceding, Unadjusted,
     )
 
 
@@ -43,7 +43,7 @@ class Frequencies(object):
 
 class Months(object):
     from quantlib.time.date import (
-        January, February, March, April, May, June, 
+        January, February, March, April, May, June,
         July, August, September, November, December,
         Jan, Feb, Mar, Apr, Jun, Jul, Aug, Sep, Oct, Nov, Dec
     )
@@ -57,17 +57,17 @@ class DayCounters(dict):
     from quantlib.time.daycounters.thirty360 import Thirty360
     from quantlib.time.daycounter import (
         Actual360, Actual365Fixed
-        )
+    )
     from quantlib.time.daycounters.actual_actual import (
         ActualActual, ISMA, ISDA, Bond
-        )
+    )
 
     _lookup = dict([(dc.name(), dc) for dc in
-        [Thirty360(), Actual360(), Actual365Fixed(),
-        ActualActual(), ActualActual(ISMA), ActualActual(ISDA),
-        ActualActual(Bond)
-        ]]
-        )
+                    [Thirty360(), Actual360(), Actual365Fixed(),
+                     ActualActual(), ActualActual(ISMA), ActualActual(ISDA),
+                     ActualActual(Bond)
+                     ]]
+                   )
 
     def __init__(self, *args):
         dict.__init__(self, self._lookup)
@@ -78,34 +78,33 @@ class DayCounters(dict):
         '''
         Calculate the fraction of a year between two date1 and date2,
         based on the daycount specified.
-    
         dates may be ccyymmdd or python datetime.dates
-    
         '''
         pydate1, pydate2 = map(pydate, (date1, date2))
-    
+
         if not daycounter:
             daycounter = cls.ActualActual()
-            
+
         qldate1 = qldate_from_pydate(pydate1)
         qldate2 = qldate_from_pydate(pydate2)
 
         return daycounter.year_fraction(qldate1, qldate2)
-        
+
     @classmethod
     def daycount(cls, date1, date2, daycounter=None):
         pydate1, pydate2 = map(pydate, (date1, date2))
-        
+
         if not daycounter:
             daycounter = cls.ActualActual()
-        
+
         qldate1 = qldate_from_pydate(pydate1)
         qldate2 = qldate_from_pydate(pydate2)
-        
+
         return daycounter.day_count(qldate1, qldate2)
-            
-                
+
+
 class Calendars(dict):
+
     '''
     Wrapper for quantlib Calendar objects and methods.
     Accepts python.datetime objects and strings
@@ -120,58 +119,62 @@ class Calendars(dict):
         
         Calendars()['TARGET'] returns TARGET calendar
         
-    ''' 
+    '''
 
     from quantlib.time.calendar import TARGET
     from quantlib.time.calendars.null_calendar import NullCalendar
     from quantlib.time.calendars.germany import (
         Germany, EUREX, FrankfurtStockExchange, SETTLEMENT as GER_SETTLEMENT,
         EUWAX, XETRA
-        )
+    )
     from quantlib.time.calendars.united_kingdom import (
-        EXCHANGE, METALS, SETTLEMENT as UK_SETTLEMENT, 
+        EXCHANGE, METALS, SETTLEMENT as UK_SETTLEMENT,
         UnitedKingdom
-        ) 
+    )
     from quantlib.time.calendars.united_states import (
         UnitedStates, GOVERNMENTBOND, NYSE, NERC, SETTLEMENT as US_SETTLEMENT
-        )
+    )
 
-    _lookup = dict([(cal.name(), cal) for cal in 
-        [TARGET(), NullCalendar(),
-         Germany(), Germany(EUREX), Germany(FrankfurtStockExchange), 
-         Germany(GER_SETTLEMENT), Germany(EUWAX), Germany(XETRA),
-         UnitedKingdom(),
-         UnitedKingdom(EXCHANGE), UnitedKingdom(METALS), 
-         UnitedKingdom(UK_SETTLEMENT),
-         UnitedStates(),
-         UnitedStates(GOVERNMENTBOND), UnitedStates(NYSE), UnitedStates(NERC), 
-         UnitedStates(US_SETTLEMENT) ]
-        ]
-        )
-    
+    _lookup = dict([(cal.name(), cal) for cal in
+                    [TARGET(), NullCalendar(),
+                     Germany(), Germany(EUREX), Germany(
+                         FrankfurtStockExchange),
+                        Germany(GER_SETTLEMENT), Germany(
+                            EUWAX), Germany(XETRA),
+                        UnitedKingdom(),
+                        UnitedKingdom(EXCHANGE), UnitedKingdom(METALS),
+                        UnitedKingdom(UK_SETTLEMENT),
+                        UnitedStates(),
+                        UnitedStates(GOVERNMENTBOND), UnitedStates(
+                            NYSE), UnitedStates(NERC),
+                        UnitedStates(US_SETTLEMENT)]
+                    ]
+                   )
+
     def __init__(self, *args):
-                       
+
         dict.__init__(self, self._lookup)
         self.update(*args)
 
     @classmethod
     def adjust(cls, date, calendar=None, convention=None):
-        
+
         if not calendar:
             calendar = cls.TARGET()
-            
+
         elif not hasattr(calendar, "adjust"):
             return None
-        
+
         if not convention:
             convention = BusinessDayConventions.Following
-        
+
         qldate = qldate_from_pydate(pydate(date))
         try:
             return pydate_from_qldate(calendar.adjust(qldate, convention))
         except:
             try:
-                return pydate_from_qldate(calendar().adjust(qldate, convention))
+                return pydate_from_qldate(calendar().adjust(qldate,
+                                                            convention))
             except:
                 return None
 
@@ -188,33 +191,33 @@ class Calendars(dict):
         -----
         
         Note 9/6/1980 is a weekend
-        
+    
         >>> Calendars.advance(19800906, 1)
         datetime.date(1980, 9, 8)
         
         """
         if not calendar:
             calendar = cls.TARGET()
-        
+
         elif not hasattr(calendar, "advance"):
             return None
-        
+
         if not convention:
             convention = BusinessDayConventions.Following
-        
+
         if not timeunit:
             timeunit = TimeUnits.Days
-            
+
         qldate = qldate_from_pydate(pydate(date))
         try:
             return pydate_from_qldate(calendar.advance(qldate, n, timeunit))
-            
+
         except:
             try:
                 return pydate_from_qldate(
                     calendar().advance(qldate, n, timeunit)
-                    )
-            
+                )
+
             except:
                 print("failure {}".format(qldate))
                 return None
@@ -223,17 +226,17 @@ class Calendars(dict):
     def is_business_day(cls, date, calendar=None):
         if not calendar:
             calendar = cls.TARGET()
-        
+
         elif not hasattr(calendar, "advance"):
             return None
-        
+
         qldate = qldate_from_pydate(pydate(date))
         try:
             return calendar.is_business_day(qldate)
-            
+
         except:
             try:
                 return calendar().is_business_day(qldate)
-            
+
             except:
-                return None            
+                return None
