e8d33c0687e820543f4291817664a6de4a2f46f6
Didrik Pinte <dpinte@enthought.com>
2014-09-12 00:00:39 +0100

    added some explanations on the skipped symbols

--- scripts/generate_symbols.py
+++ scripts/generate_symbols.py
@@ -1,30 +1,60 @@
 """ Symbol management to generate .def file. """
 import glob
 import os
-from subprocess import check_output
+import subprocess
 
-def get_symbol_from_obj_file(object_file):
+def symbol_generator_from_obj_file(object_file):
     
-    command = ['nm', '-g', '--defined-only', object_file]
+    command = ['nm', '--extern-only', '--defined-only', object_file]
     
-    result = check_output(command, shell=False)
+    # don't show a window when executing the subprocess
+    startupinfo = subprocess.STARTUPINFO()
+    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
     
-    for line in result.split('\n'):
+    nm_result = subprocess.check_output(command, startupinfo=startupinfo)
+    
+    def _is_ql_symbols(symb):
+        # Includes all the symbols containing QuantLib except the ones with 
+        # AVError and all the scalar deleting destructor (prefixed with _G)
+        # and all the vector deleting destructor (prefixed with a _E)
+        # and ?AVError (not yet sure what cause the problem :
+        # (Error 1 error LNK2001: unresolved external symbol 
+        # "??_R0?AVError@QuantLib@@@8??0Error@QuantLib@@QAE@ABV01@@Z20"
+        # (??_R0?AVError@QuantLib@@@8??0Error@QuantLib@@QAE@ABV01@@Z20)
+        # QuantLib.exp	QuantLib
+        # )
+        # See http://en.wikipedia.org/wiki/Visual_C%2B%2B_name_mangling for
+        # reference
+        return (
+            'QuantLib' in symb and 
+            '?AVError' not in symb and
+            '??_G' not in symb and
+            '??_E' not in symb
+        )
+        
+    def _is_boost_assertion(symb):
+        return '?assertion_failed@boost' in mangled_symbol
+        
+    for line in nm_result.split('\n'):
+        # Example line: 
+        # 0000000000000000 R ?value@?$integral_constant@_N$00@tr1@std@@2_NB
+        # Find the symbol location in the line
         idx = line.find('?')
+        
+        # Get the symbol type
         stat = line[idx-2:idx].strip()
-        if stat == 'w':
-            print 'Skipping ', line
+        if stat == 'w': # skip weak symbols
             continue
+            
         mangled_symbol =  line[idx:].strip()
+        
         if len(mangled_symbol) > 0:
-            if 'QuantLib' in mangled_symbol and 'AVError' not in mangled_symbol:
-                yield mangled_symbol
-            elif '?assertion_failed@boost' in mangled_symbol:
+            if _is_ql_symbols(mangled_symbol) or _is_boost_assertion(mangled_symbol):
                 yield mangled_symbol
             
 def process_directory(directory_name):
     for object_file in glob.glob(os.path.join(directory_name, '*.obj')):
-        for symbol in get_symbol_from_obj_file(object_file):
+        for symbol in symbol_generator_from_obj_file(object_file):
             yield symbol
     
 HEADER = """LIBRARY	"QuantLib"
@@ -32,19 +62,25 @@ EXPORTS
 """
 
 def test():
-    test_directory = r"C:\dev\QuantLib-1.4\build\vc90\Win32\Release"
-    test_file = os.path.join(test_directory, "zigguratrng.obj")
-    output_file = r'C:\dev\pyql\symbols_win32.def'
+    test_directory = r"C:\dev\QuantLib-1.4\build\vc90\x64\Release"
+    test_file = os.path.join(test_directory, "zeroyieldstructure.obj")
+
+    for obj in symbol_generator_from_obj_file(test_file):
+        pass
     
-    for obj in get_symbol_from_obj_file(test_file):
-        print obj
+def generate_deffile_from_dir(input_directory, output_file):
     
     with open(output_file, 'w') as fh:
         fh.write(HEADER)
-        for symbol in process_directory(test_directory):
+        for symbol in process_directory(input_directory):
             fh.write('	 {}\n'.format(symbol))
     
-
-if __name__ == '__main__':
-   test()
+def main():
+    input_directory = r"C:\dev\QuantLib-1.4\build\vc90\Win32\Release"        
+    output_file = r'C:\dev\pyql\symbols_win32.def'
+        
+    generate_deffile_from_dir(input_directory, output_file)
+    print '{} generated'.format(output_file)
     
+if __name__ == '__main__':
+   main() 
