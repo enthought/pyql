905fe0b786f8d979796f0f60932f8ced5ea89299
Abraham Lopez <alopez@rimrockcapital.com>
2015-04-30 16:29:49 -0700

    Extending term structure interface to include forward spreaded termstructure

---
+++ quantlib/termstructures/yields/forward_spreaded_term_structure.pyx
@@ -0,0 +1,40 @@
+include '../../types.pxi'
+
+from cython.operator cimport dereference as deref
+from libcpp.vector cimport vector
+
+cimport quantlib.termstructures.yields._forward_spreaded_term_structure as _fsts
+cimport quantlib.termstructures.yields._flat_forward as _ff
+cimport quantlib._quote as _qt
+
+from quantlib.handle cimport shared_ptr, Handle
+from quantlib.termstructures.yields.flat_forward cimport YieldTermStructure
+from quantlib.time.daycounter cimport DayCounter
+from quantlib.quotes cimport Quote
+from quantlib.time.date cimport Date
+
+cdef class ForwardSpreadedTermStructure(YieldTermStructure):
+    """
+        Constructor Inputs: 
+           1. YieldTermStructure
+           2. Quote
+        
+    """
+    def __init__(self,YieldTermStructure yldtermstruct, Quote spread):
+
+        # convert inputs to handles
+        
+        cdef Handle[_qt.Quote] q_handle = Handle[_qt.Quote](deref(spread._thisptr))
+
+        cdef Handle[_ff.YieldTermStructure] yts_handle = deref(yldtermstruct._thisptr.get())
+        #cdef shared_ptr[_ff.YieldTermStructure] _yts = shared_ptr[_ff.YieldTermStructure](yts_handle.currentLink())
+        cdef _fsts.ForwardSpreadedTermStructure* _fwdts
+
+        _fwdts = new _fsts.ForwardSpreadedTermStructure(
+                    yts_handle, 
+                    q_handle
+                    )
+        
+        self._thisptr = new shared_ptr[Handle[_ff.YieldTermStructure]](
+             new Handle[_ff.YieldTermStructure](shared_ptr[_ff.YieldTermStructure](_fwdts))
+        )
