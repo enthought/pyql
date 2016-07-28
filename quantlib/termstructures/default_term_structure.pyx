4cdd748f49c94551e1f86f1e3a16b09bfbabf6cb
Guillaume Horel <guillaume.horel@serenitascapital.com>
2016-07-28 13:58:58 -0400

    add a couple of methods

--- quantlib/termstructures/default_term_structure.pyx
+++ quantlib/termstructures/default_term_structure.pyx
@@ -2,8 +2,11 @@ include '../types.pxi'
 
 from libcpp cimport bool
 from cython.operator cimport dereference as deref
-from quantlib.time.date cimport Date
+from quantlib.time.date cimport Date, date_from_qldate
+cimport quantlib.time._date as _date
 from quantlib.time.calendar cimport DateList
+from quantlib.time.daycounter cimport DayCounter
+cimport quantlib.time._daycounter as _daycounter
 
 cdef class DefaultProbabilityTermStructure: #not inheriting from TermStructure at this point
 
@@ -38,3 +41,19 @@ cdef class DefaultProbabilityTermStructure: #not inheriting from TermStructure a
 
     def time_from_reference(self, Date d):
         return self._thisptr.get().timeFromReference(deref(d._thisptr.get()))
+
+    @property
+    def max_date(self):
+        cdef _date.Date date = self._thisptr.get().maxDate()
+        return date_from_qldate(date)
+
+    @property
+    def reference_date(self):
+        cdef _date.Date date = self._thisptr.get().referenceDate()
+        return date_from_qldate(date)
+
+    @property
+    def day_counter(self):
+        cdef DayCounter dc = DayCounter()
+        dc._thisptr = new _daycounter.DayCounter(self._thisptr.get().dayCounter())
+        return dc
