581ffbd3888b8ca641f4d9591fa582a092c43ff1
Guillaume Horel <guillaume.horel@gmail.com>
2017-02-06 14:11:25 -0500

    add UKRPI

---
+++ quantlib/indexes/inflation/urkpi.pyx
@@ -0,0 +1,18 @@
+from libcpp cimport bool
+
+from quantlib.currency.api import GBPCurrency
+from quantlib.indexes.regions import UKRegion
+from quantlib.time.date cimport Period
+from quantlib.time.date import Monthly, Months
+
+from quantlib.indexes.inflation_index cimport ZeroInflationIndex
+from quantlib.termstructures.inflation_term_structure \
+    cimport ZeroInflationTermStructure
+
+cdef class UKRPI(ZeroInflationIndex):
+    def __init__(self, bool interpolated,
+                 ZeroInflationTermStructure ts=ZeroInflationTermStructure()):
+
+        super().__init__("RPI", UKRegion(), False,
+                         interpolated, Monthly, Period(1, Months),
+                         GBPCurrency(), ts)
