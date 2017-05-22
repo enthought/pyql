fef3fec771e863bc64feb46177c4992be4aed996
Guillaume Horel <guillaume.horel@gmail.com>
2017-05-22 15:50:45 -0400

    more inflation indices

---
+++ quantlib/indexes/inflation/euhicp.pyx
@@ -0,0 +1,43 @@
+from libcpp cimport bool
+
+from quantlib.currency.api import EURCurrency
+from quantlib.indexes.regions import EURegion
+from quantlib.indexes.inflation_index cimport (
+    ZeroInflationIndex, YoYInflationIndex)
+from quantlib.time._period cimport Monthly, Months
+from quantlib.time.date cimport Period
+from quantlib.termstructures.inflation_term_structure cimport (
+    ZeroInflationTermStructure, YoYInflationTermStructure)
+
+cdef class EUHICP(ZeroInflationIndex):
+    def __init__(self, bool interpolated,
+                 ZeroInflationTermStructure ts=ZeroInflationTermStructure()):
+
+        super().__init__("HICP", EURegion(), False,
+                         interpolated, Monthly, Period(1, Months),
+                         EURCurrency(), ts)
+
+
+cdef class EUHICPXT(ZeroInflationIndex):
+    def __init__(self, bool interpolated,
+                 ZeroInflationTermStructure ts=ZeroInflationTermStructure()):
+
+        super().__init__("HICPXT", EURegion(), False,
+                         interpolated, Monthly, Period(1, Months),
+                         EURCurrency(), ts)
+
+cdef class YYEUHICP(YoYInflationIndex):
+    def __init__(self, bool interpolated,
+                 YoYInflationTermStructure ts=YoYInflationTermStructure()):
+
+        super().__init__("YY_HICP", EURegion(), False,
+                         interpolated, Monthly, Period(1, Months),
+                         EURCurrency(), ts)
+
+cdef class YYEUHICPXT(YoYInflationIndex):
+    def __init__(self, bool interpolated,
+                 YoYInflationTermStructure ts=YoYInflationTermStructure()):
+
+        super().__init__("YY_HICPXT", EURegion(), False,
+                         interpolated, Monthly, Period(1, Months),
+                         EURCurrency(), ts)
