594ad7c9856e77ebd34bef9c4ff621402bfc9c5f
Guillaume Horel <guillaume.horel@gmail.com>
2017-09-11 13:51:17 -0400

    cleanup Interpolation types

---
+++ quantlib/math/interpolation.pxd
@@ -0,0 +1,9 @@
+cdef extern from 'ql/math/interpolations/all.hpp' namespace 'QuantLib':
+    cdef cppclass Linear:
+        pass
+
+    cdef cppclass LogLinear:
+        pass
+
+    cdef cppclass BackwardFlat:
+        pass
--- quantlib/termstructures/credit/interpolated_hazardrate_curve.pyx
+++ quantlib/termstructures/credit/interpolated_hazardrate_curve.pyx
@@ -4,10 +4,9 @@ from quantlib.handle cimport shared_ptr
 
 include '../../types.pxi'
 
-from quantlib.termstructures.default_term_structure cimport DefaultProbabilityTermStructure
-
 cimport quantlib.termstructures.credit._interpolated_hazardrate_curve as _ihc
 cimport quantlib.termstructures._default_term_structure as _dts
+cimport quantlib.math.interpolation as intpl
 from quantlib.time.date cimport Date, date_from_qldate
 cimport quantlib.time._date as _date
 
@@ -31,40 +30,35 @@ cdef class InterpolatedHazardRateCurve(DefaultProbabilityTermStructure):
 
     """
     def __init__(self, Interpolator interpolator, list dates, vector[Rate] hazard_rates,
-                 DayCounter day_counter not None, Calendar cal = None):
+                 DayCounter day_counter not None, Calendar cal=Calendar()):
         # convert the list of PyQL dates into a vector of QL dates
         cdef vector[_date.Date] _dates
         for date in dates:
             _dates.push_back(deref((<Date?>date)._thisptr.get()))
 
-        cdef _calendar.Calendar c_cal
-        if cal is None:
-            c_cal = _calendar.Calendar()
-        else:
-            c_cal = deref(cal._thisptr)
 
         self._trait = interpolator
 
         if interpolator == Linear:
             self._thisptr = shared_ptr[_dts.DefaultProbabilityTermStructure](
-                new _ihc.InterpolatedHazardRateCurve[_ihc.Linear](
+                new _ihc.InterpolatedHazardRateCurve[intpl.Linear](
                     _dates, hazard_rates,
                     deref(day_counter._thisptr),
-                    c_cal)
+                    deref(cal._thisptr))
             )
         elif interpolator == LogLinear:
             self._thisptr = shared_ptr[_dts.DefaultProbabilityTermStructure](
-                new _ihc.InterpolatedHazardRateCurve[_ihc.LogLinear](
+                new _ihc.InterpolatedHazardRateCurve[intpl.LogLinear](
                     _dates, hazard_rates,
                     deref(day_counter._thisptr),
-                    c_cal)
+                    deref(cal._thisptr))
             )
         elif interpolator == BackwardFlat:
             self._thisptr = shared_ptr[_dts.DefaultProbabilityTermStructure](
-                new _ihc.InterpolatedHazardRateCurve[_ihc.BackwardFlat](
+                new _ihc.InterpolatedHazardRateCurve[intpl.BackwardFlat](
                     _dates, hazard_rates,
                     deref(day_counter._thisptr),
-                    c_cal)
+                    deref(cal._thisptr))
             )
         else:
             raise ValueError("interpolator needs to be any of Linear, LogLinear or BackwardFlat")
@@ -74,13 +68,13 @@ cdef class InterpolatedHazardRateCurve(DefaultProbabilityTermStructure):
         """list of curve dates"""
         cdef vector[_date.Date] _dates
         if self._trait == Linear:
-            _dates = (<_ihc.InterpolatedHazardRateCurve[_ihc.Linear]*>
+            _dates = (<_ihc.InterpolatedHazardRateCurve[intpl.Linear]*>
                       self._thisptr.get()).dates()
         elif self._trait == LogLinear:
-            _dates = (<_ihc.InterpolatedHazardRateCurve[_ihc.LogLinear]*>
+            _dates = (<_ihc.InterpolatedHazardRateCurve[intpl.LogLinear]*>
                        self._thisptr.get()).dates()
         else:
-            _dates = (<_ihc.InterpolatedHazardRateCurve[_ihc.BackwardFlat]*>
+            _dates = (<_ihc.InterpolatedHazardRateCurve[intpl.BackwardFlat]*>
                       self._thisptr.get()).dates()
         cdef size_t i
         cdef list r  = []
@@ -93,37 +87,37 @@ cdef class InterpolatedHazardRateCurve(DefaultProbabilityTermStructure):
     def times(self):
         """list of curve times"""
         if self._trait == Linear:
-            return (<_ihc.InterpolatedHazardRateCurve[_ihc.Linear]*>
+            return (<_ihc.InterpolatedHazardRateCurve[intpl.Linear]*>
                     self._thisptr.get()).times()
         elif self._trait == LogLinear:
-            return (<_ihc.InterpolatedHazardRateCurve[_ihc.LogLinear]*>
+            return (<_ihc.InterpolatedHazardRateCurve[intpl.LogLinear]*>
                     self._thisptr.get()).times()
         else:
-            return (<_ihc.InterpolatedHazardRateCurve[_ihc.BackwardFlat]*>
+            return (<_ihc.InterpolatedHazardRateCurve[intpl.BackwardFlat]*>
                     self._thisptr.get()).times()
 
     @property
     def data(self):
         """list of curve data"""
         if self._trait == Linear:
-            return (<_ihc.InterpolatedHazardRateCurve[_ihc.Linear]*>
+            return (<_ihc.InterpolatedHazardRateCurve[intpl.Linear]*>
                     self._thisptr.get()).data()
         elif self._trait == LogLinear:
-            return (<_ihc.InterpolatedHazardRateCurve[_ihc.LogLinear]*>
+            return (<_ihc.InterpolatedHazardRateCurve[intpl.LogLinear]*>
                     self._thisptr.get()).data()
         else:
-            return (<_ihc.InterpolatedHazardRateCurve[_ihc.BackwardFlat]*>
+            return (<_ihc.InterpolatedHazardRateCurve[intpl.BackwardFlat]*>
                      self._thisptr.get()).data()
 
     @property
     def hazard_rates(self):
         """list of curve hazard rates"""
         if self._trait == Linear:
-            return (<_ihc.InterpolatedHazardRateCurve[_ihc.Linear]*>
+            return (<_ihc.InterpolatedHazardRateCurve[intpl.Linear]*>
                 self._thisptr.get()).hazardRates()
         elif self._trait == LogLinear:
-            return (<_ihc.InterpolatedHazardRateCurve[_ihc.LogLinear]*>
+            return (<_ihc.InterpolatedHazardRateCurve[intpl.LogLinear]*>
                 self._thisptr.get()).hazardRates()
         else:
-            return (<_ihc.InterpolatedHazardRateCurve[_ihc.BackwardFlat]*>
+            return (<_ihc.InterpolatedHazardRateCurve[intpl.BackwardFlat]*>
                     self._thisptr.get()).hazardRates()
