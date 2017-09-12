8f9187ab4d59e9f0f5c4946d746f363fd94ee833
Guillaume Horel <guillaume.horel@gmail.com>
2017-09-12 17:43:21 -0400

    do not use class variable

--- quantlib/test/test_sabr.py
+++ quantlib/test/test_sabr.py
@@ -10,17 +10,18 @@ from quantlib.termstructures.volatility.sabr import unsafe_sabr_volatility
 import numpy as np
 
 class SabrTestCase(unittest.TestCase):
-    option_date = Date(20, 9, 2017)
-    Settings().evaluation_date = Date(4, 8, 2017)
-    strikes = np.array([50, 55, 57.5, 60, 62.5, 65, 67.5, 70, 75, 80, 85, 90, 95, 100]) * 1e-4
-    vol = np.array([28.5, 31.6, 33.7, 36.1, 38.7, 41.5, 44.1,
-                    46.5, 50.8, 54.4, 57.3, 59.8, 61.8, 63.6]) * 1e-2
-    vol_quotes = [SimpleQuote(q) for q in vol]
-    forward = SimpleQuote(58.71e-4)
-    atm_vol = (60-58.71)/1.5*33.7 + (58.71-57.5)/1.5*36.1
-    sabr_smile = SabrInterpolatedSmileSection(option_date, forward, strikes, False,
-                                              SimpleQuote(0.4), vol_quotes, 0.1, 1, 0.1, 0.5,
-                                              is_beta_fixed=True)
+    def setUp(self):
+        option_date = Date(20, 9, 2017)
+        Settings().evaluation_date = Date(4, 8, 2017)
+        self.strikes = np.array([50, 55, 57.5, 60, 62.5, 65, 67.5, 70, 75, 80, 85, 90, 95, 100]) * 1e-4
+        vol = np.array([28.5, 31.6, 33.7, 36.1, 38.7, 41.5, 44.1,
+                        46.5, 50.8, 54.4, 57.3, 59.8, 61.8, 63.6]) * 1e-2
+        vol_quotes = [SimpleQuote(q) for q in vol]
+        self.forward = SimpleQuote(58.71e-4)
+        atm_vol = (60-58.71)/1.5*33.7 + (58.71-57.5)/1.5*36.1
+        self.sabr_smile = SabrInterpolatedSmileSection(option_date, self.forward, self.strikes, False,
+                                                       SimpleQuote(0.4), vol_quotes, 0.1, 1, 0.1, 0.5,
+                                                       is_beta_fixed=True)
     def test_params(self):
         alpha, rho, nu = self.sabr_smile.alpha, self.sabr_smile.rho, self.sabr_smile.nu
         self.assertTrue(alpha > 0)
