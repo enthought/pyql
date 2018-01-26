37235c5f76e0b917aa41e09e2fae3f9cf1f51298
Guillaume Horel <guillaume.horel@gmail.com>
2018-01-26 17:01:26 -0500

    cleanup instrument

--- quantlib/instruments/instrument.pxd
+++ quantlib/instruments/instrument.pxd
@@ -1,11 +1,5 @@
 from quantlib.handle cimport shared_ptr
-from quantlib.pricingengines.engine cimport PricingEngine
-from libcpp cimport bool as cbool
 cimport _instrument
 
-cimport quantlib.pricingengines._pricing_engine as _pe
-
 cdef class Instrument:
-
-    cdef cbool _has_pricing_engine
     cdef shared_ptr[_instrument.Instrument]* _thisptr
--- quantlib/instruments/instrument.pyx
+++ quantlib/instruments/instrument.pyx
@@ -1,10 +1,6 @@
 from cython.operator cimport dereference as deref
-
-from quantlib.handle cimport shared_ptr
+from quantlib.time.date cimport date_from_qldate
 from quantlib.pricingengines.engine cimport PricingEngine
-
-cimport quantlib.pricingengines._pricing_engine as _pe
-cimport _instrument
 
 cdef class Instrument:
 
@@ -17,21 +13,23 @@ cdef class Instrument:
             del self._thisptr
             self._thisptr = NULL
 
-    def set_pricing_engine(self, PricingEngine engine):
+    def set_pricing_engine(self, PricingEngine engine not None):
         '''Sets the pricing engine.
 
         '''
         self._thisptr.get().setPricingEngine(deref(engine._thisptr))
 
-        self._has_pricing_engine = True
 
     property net_present_value:
         """ Instrument net present value. """
         def __get__(self):
-            if self._has_pricing_engine:
-                return self._thisptr.get().NPV()
+            return self._thisptr.get().NPV()
 
     property npv:
         """ Shortcut to the net_present_value property. """
         def __get__(self):
-            return self.net_present_value
+            return self._thisptr.get().NPV()
+
+    @property
+    def valuation_date(self):
+        return date_from_qldate(self._thisptr.get().valuationDate())
