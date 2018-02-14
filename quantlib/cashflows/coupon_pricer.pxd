4b4d2bbdad3f563ed0091547d4555b1e98b99e8b
Guillaume Horel <guillaume.horel@gmail.com>
2018-02-14 14:53:26 -0500

    cleanup coupon_pricer

--- quantlib/cashflows/coupon_pricer.pxd
+++ quantlib/cashflows/coupon_pricer.pxd
@@ -2,10 +2,7 @@ from quantlib.handle cimport shared_ptr
 cimport _coupon_pricer as _cp
 
 cdef class FloatingRateCouponPricer:
-    cdef shared_ptr[_cp.FloatingRateCouponPricer]* _thisptr
+    cdef shared_ptr[_cp.FloatingRateCouponPricer] _thisptr
 
 cdef class IborCouponPricer(FloatingRateCouponPricer):
     pass
-
-
-
--- quantlib/cashflows/coupon_pricer.pyx
+++ quantlib/cashflows/coupon_pricer.pyx
@@ -1,3 +1,5 @@
+include '../types.pxi'
+
 from cython.operator cimport dereference as deref
 cimport _coupon_pricer as _cp
 cimport quantlib.instruments._bonds as _bonds
@@ -7,46 +9,73 @@ from quantlib.handle cimport Handle
 from quantlib.time.calendar cimport Calendar
 from quantlib.time.date cimport Date
 from quantlib.time.daycounter cimport DayCounter
+from quantlib.quotes cimport SimpleQuote
 from coupon_pricer cimport FloatingRateCouponPricer
 from quantlib.instruments.bonds cimport Bond
 cimport quantlib._cashflow as _cf
+cimport quantlib._quote as _qt
 
-cdef class IborCouponPricer:
-
-    def __cinit__(self):
-        self._thisptr = NULL
-
-    def __dealloc__(self):
-        if self._thisptr is not NULL:
-            del self._thisptr
+cdef class FloatingRateCouponPricer:
 
     def __init__(self):
         raise ValueError(
             'IborCouponPricer cannot be directly instantiated!'
         )
 
+    def swaplet_price(self):
+        return self._thisptr.get().swapletPrice()
+
+    def swaplet_rate(self):
+        return self._thisptr.get().swapletRate()
+
+    def caplet_price(self, Rate effective_cap):
+        return self._thisptr.get().capletPrice(effective_cap)
+
+    def caplet_rate(self, Rate effective_cap):
+        return self._thisptr.get().capletRate(effective_cap)
+
+    def floorlet_price(self, Rate effective_floor):
+        return self._thisptr.get().floorletPrice(effective_floor)
+
+    def floorlet_rate(self, Rate effective_floor):
+        return self._thisptr.get().floorletRate(effective_floor)
+
+
+cdef class IborCouponPricer(FloatingRateCouponPricer):
+
+    def __init__(self):
+        raise ValueError(
+            'IborCouponPricer cannot be directly instantiated!'
+        )
+
+
+cpdef enum TimingAdjustment:
+    Black76 = _cp.Black76
+    BivariateLognormal = _cp.BivariateLognormal
+
 cdef class BlackIborCouponPricer(IborCouponPricer):
 
     def __init__(self,
-        OptionletVolatilityStructure ovs
-    ):
-        ovs_handle = new Handle[_ovs.OptionletVolatilityStructure](deref(ovs._thisptr))
-        self._thisptr = new shared_ptr[_cp.FloatingRateCouponPricer](
+                 OptionletVolatilityStructure ovs=OptionletVolatilityStructure(),
+                 TimingAdjustment timing_adjustment=Black76,
+                 SimpleQuote correlation=SimpleQuote(1.)):
+        cdef Handle[_ovs.OptionletVolatilityStructure] ovs_handle = \
+            Handle[_ovs.OptionletVolatilityStructure](ovs._thisptr)
+        cdef Handle[_qt.Quote] correlation_handle = Handle[_qt.Quote](correlation._thisptr)
+        self._thisptr = shared_ptr[_cp.FloatingRateCouponPricer](
             new _cp.BlackIborCouponPricer(
-                deref(ovs_handle)
+                ovs_handle,
+                timing_adjustment,
+                correlation_handle
             )
         )
 
-def set_coupon_pricer(Bond frb, pricer):
+def set_coupon_pricer(Bond frb, FloatingRateCouponPricer pricer):
     """ Parameters :
         ----------
-        1) frb : FloatingRateBond  
+        1) frb : FloatingRateBond
             Bond object to be used to extract cashflows
-        2) pricer : FloatingRateCouponPricer 
+        2) pricer : FloatingRateCouponPricer
             BlackIborCouponPricer has been exposed"""
-    cdef shared_ptr[_cp.FloatingRateCouponPricer] pricer_sp
-    pricer_sp = deref((<FloatingRateCouponPricer>pricer)._thisptr)
     bond_leg = (<_bonds.Bond*>frb._thisptr.get()).cashflows()
-    _cp.setCouponPricer(bond_leg, pricer_sp)
-    
-    
+    _cp.setCouponPricer(bond_leg, pricer._thisptr)
