eabf978ef5fb6be2525bf71a468a2e7337857d31
Guillaume Horel <guillaume.horel@gmail.com>
2018-03-02 12:34:07 -0500

    return ndarray directly

--- quantlib/math/randomnumbers/rngtraits.pyx
+++ quantlib/math/randomnumbers/rngtraits.pyx
@@ -1,4 +1,9 @@
 include '../../types.pxi'
+cimport cython
+cimport numpy as np
+from cython.operator cimport dereference as deref
+from libcpp.vector cimport vector
+np.import_array()
 
 cdef class LowDiscrepancy:
 
@@ -13,8 +18,17 @@ cdef class LowDiscrepancy:
     def __iter__(self):
         return self
 
+    @cython.boundscheck(False)
     def __next__(self):
-        return self._thisptr.nextSequence().value
+        cdef np.npy_intp[1] dims
+        dims[0] = self._thisptr.dimension()
+        cdef arr = np.PyArray_SimpleNew(1, &dims[0], np.NPY_DOUBLE)
+        cdef double[:] r = arr
+        cdef size_t i
+        cdef vector[double] s = self._thisptr.nextSequence().value
+        for i in range(dims[0]):
+            r[i] = s[i]
+        return arr
 
     @property
     def dimension(self):
