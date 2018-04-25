a0c3ea006062152504df612d8b561923762fdbfb
Guillaume Horel <guillaume.horel@gmail.com>
2018-04-25 17:22:35 -0400

    add methods

--- quantlib/instruments/_swaption.pxd
+++ quantlib/instruments/_swaption.pxd
@@ -3,6 +3,9 @@ from quantlib.handle cimport shared_ptr
 from ._vanillaswap cimport VanillaSwap
 from ._option cimport Option
 from ._exercise cimport Exercise
+from quantlib.termstructures._yield_term_structure cimport YieldTermStructure
+from quantlib.termstructures.volatility.volatilitytype cimport VolatilityType
+from quantlib.handle cimport Handle
 
 cdef extern from 'ql/instruments/swaption.hpp' namespace 'QuantLib':
     cdef cppclass Settlement:
@@ -14,3 +17,14 @@ cdef extern from 'ql/instruments/swaption.hpp' namespace 'QuantLib':
         Swaption(const shared_ptr[VanillaSwap]& swap,
                  const shared_ptr[Exercise]& exercise,
                  Settlement.Type delivery)# = Settlement.Physical)
+        Settlement.Type settlementType()
+        Volatility impliedVolatility(Real price,
+                                     const Handle[YieldTermStructure]& discountCurve,
+                                     Volatility guess,
+                                     Real accuracy,# = 1.0e-4,
+                                     Natural maxEvaluations,# = 100,
+                                     Volatility minVol,# = 1.0e-7,
+                                     Volatility maxVol,# = 4.0,
+                                     VolatilityType type,# = ShiftedLognormal,
+                                     Real displacement)# = 0.0)
+        const shared_ptr[VanillaSwap]& underlyingSwap()
--- quantlib/instruments/swaption.pyx
+++ quantlib/instruments/swaption.pyx
@@ -1,5 +1,9 @@
+include '../types.pxi'
 cimport _swaption
 from quantlib.handle cimport shared_ptr, static_pointer_cast
+from quantlib.termstructures.volatility.volatilitytype cimport (
+    VolatilityType, ShiftedLognormal )
+from quantlib.termstructures.yield_term_structure cimport YieldTermStructure
 from .option cimport Exercise
 from .swap cimport VanillaSwap
 cimport _vanillaswap
@@ -17,3 +21,34 @@ cdef class Swaption(Instrument):
                 static_pointer_cast[_vanillaswap.VanillaSwap](swap._thisptr),
                 exercise._thisptr,
                 <Settlement.Type>delivery))
+
+    def implied_volatility(self, Real price,
+                           YieldTermStructure discount_curve not None,
+                           Volatility guess,
+                           Real accuracy=1e-4,
+                           Natural max_evaluations=100,
+                           Volatility min_vol=1e-7,
+                           Volatility max_vol=4.,
+                           VolatilityType type=ShiftedLognormal,
+                           Real displacement=0.):
+        return (<_swaption.Swaption*>self._thisptr.get()).impliedVolatility(
+            price,
+            discount_curve._thisptr,
+            guess,
+            accuracy,
+            max_evaluations,
+            min_vol,
+            max_vol,
+            type,
+            displacement)
+
+    @property
+    def settlement_type(self):
+        return SettlementType((<_swaption.Swaption*>self._thisptr.get()).settlementType())
+
+    def underlying_swap(self):
+        cdef VanillaSwap instance = VanillaSwap.__new__(VanillaSwap)
+        instance._thisptr = static_pointer_cast[_instrument.Instrument](
+            (<_swaption.Swaption*>self._thisptr.get()).
+            underlyingSwap())
+        return instance
