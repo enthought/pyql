170ce2ac47f4c54c049a9f0be123bc8c9fe81c9d
Guillaume Horel <guillaume.horel@gmail.com>
2018-05-23 16:41:46 -0400

    turn some methods into properties

--- quantlib/termstructures/volatility/swaption/swaption_vol_discrete.pyx
+++ quantlib/termstructures/volatility/swaption/swaption_vol_discrete.pyx
@@ -3,11 +3,14 @@ from cython.operator cimport preincrement as preinc, dereference as deref
 from quantlib.time.date cimport period_from_qlperiod, date_from_qldate
 cimport quantlib.time._period as _period
 cimport quantlib.time._date as _date
+
+
 cdef inline _svd.SwaptionVolatilityDiscrete* _get_svd(SwaptionVolatilityDiscrete vs):
     return <_svd.SwaptionVolatilityDiscrete*> vs._thisptr.get()
 
 cdef class SwaptionVolatilityDiscrete(SwaptionVolatilityStructure):
 
+    @property
     def option_tenors(self):
         cdef vector[_period.Period].const_iterator it = \
                 _get_svd(self).optionTenors().const_begin()
@@ -17,6 +20,7 @@ cdef class SwaptionVolatilityDiscrete(SwaptionVolatilityStructure):
             preinc(it)
         return r
 
+    @property
     def option_dates(self):
         cdef vector[_date.Date].const_iterator it = \
                 _get_svd(self).optionDates().const_begin()
@@ -25,9 +29,11 @@ cdef class SwaptionVolatilityDiscrete(SwaptionVolatilityStructure):
             r.append(date_from_qldate(deref(it)))
             preinc(it)
 
+    @property
     def option_times(self):
         return _get_svd(self).optionTimes()
 
+    @property
     def swap_tenors(self):
         cdef vector[_period.Period].const_iterator it = \
                 _get_svd(self).swapTenors().const_begin()
@@ -37,5 +43,6 @@ cdef class SwaptionVolatilityDiscrete(SwaptionVolatilityStructure):
             preinc(it)
         return r
 
+    @property
     def swap_lengths(self):
         return _get_svd(self).swapLengths()
