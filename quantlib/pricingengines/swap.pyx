3a531dfdba81322263e77990e8610c3252043bb4
Guillaume Horel <guillaume.horel@gmail.com>
2018-07-27 14:28:07 -0400

    fix DiscountingSwapEngine

--- quantlib/pricingengines/swap.pyx
+++ quantlib/pricingengines/swap.pyx
@@ -1,7 +1,7 @@
 from cython.operator cimport dereference as deref
 
 from libcpp cimport bool
-from quantlib.handle cimport shared_ptr, Handle
+from quantlib.handle cimport shared_ptr, optional
 from quantlib.pricingengines.vanilla.vanilla cimport PricingEngine
 from quantlib.termstructures.yield_term_structure cimport YieldTermStructure
 
@@ -12,26 +12,18 @@ from quantlib.time.date cimport Date
 
 cdef class DiscountingSwapEngine(PricingEngine):
 
-    def __init__(self, YieldTermStructure discount_curve,
-                 includeSettlementDateFlows=None,
-                 Date settlementDate=None,
-                 Date npvDate=None):
-
-        if includeSettlementDateFlows is None and settlementDate is None and \
-           npvDate is None:
-            self._thisptr = new shared_ptr[_pe.PricingEngine](
-                new _swap.DiscountingSwapEngine(discount_curve._thisptr)
+    def __init__(self, YieldTermStructure discount_curve not None,
+                 include_settlement_date_flows=None,
+                 Date settlement_date=Date(),
+                 Date npv_date=Date()):
+        cdef optional[bool] include_settlement_date_flows_opt
+        if include_settlement_date_flows is not None:
+            include_settlement_date_flows_opt = <bool>include_settlement_date_flows
+        self._thisptr = new shared_ptr[_pe.PricingEngine](
+            new _swap.DiscountingSwapEngine(
+                discount_curve._thisptr,
+                include_settlement_date_flows_opt,
+                deref(settlement_date._thisptr),
+                deref(npv_date._thisptr)
             )
-        elif includeSettlementDateFlows is not None and \
-             settlementDate is not None and \
-             npvDate is not None:
-            self._thisptr = new shared_ptr[_pe.PricingEngine](
-                new _swap.DiscountingSwapEngine(
-                    discount_curve._thisptr,
-                    includeSettlementDateFlows,
-                    deref(settlementDate._thisptr.get()),
-                    deref(npvDate._thisptr.get())
-                )
-            )
-        else:
-            raise NotImplementedError('Constructor not yet implemented')
+        )
