f5f0c2ac1979a3816dc75d1010d8492f0aa999ed
Guillaume Horel <guillaume.horel@gmail.com>
2018-08-07 17:50:49 -0400

    allow to observe evaluation_date

--- quantlib/test/test_observable.py
+++ quantlib/test/test_observable.py
@@ -1,13 +1,15 @@
 import unittest
 from quantlib.quotes import SimpleQuote
 from quantlib.observable import Observer
-
+from quantlib.settings import Settings
+from quantlib.time.api import Date
 
 class SimpleObserverTestCase(unittest.TestCase):
     def setUp(self):
         self.count = 0
 
-    def test_observability(self):
+    def test_quote_observability(self):
+        self.count = 0
         q = SimpleQuote(0.1)
 
         def counter():
@@ -22,6 +24,19 @@ class SimpleObserverTestCase(unittest.TestCase):
         self.assertEqual(self.count, 2)
         obs.unregister_with(q)
         q.value = 0.4
+        self.assertEqual(self.count, 2)
+
+    def test_settings_observability(self):
+        self.count = 0
+        def counter():
+            self.count += 1
+        obs = Observer(counter)
+        obs.register_with(Settings().observable_evaluation_date)
+
+        self.assertEqual(self.count, 0)
+        with Settings() as settings:
+            settings.evaluation_date = Date(1, 1, 2018)
+            self.assertEqual(self.count, 1)
         self.assertEqual(self.count, 2)
 
 if __name__ == "__main__":
