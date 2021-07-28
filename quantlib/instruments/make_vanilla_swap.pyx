44d971acc553d50e178e7353077c8d5ade32aee0
Guillaume Horel <guillaume.horel@gmail.com>
2021-07-28 05:16:08 -0400

    extend MakeVanilla

--- quantlib/instruments/make_vanilla_swap.pyx
+++ quantlib/instruments/make_vanilla_swap.pyx
@@ -6,8 +6,11 @@ from quantlib.handle cimport static_pointer_cast, shared_ptr
 from quantlib.indexes.ibor_index cimport IborIndex
 from quantlib.time.date cimport Period, Date
 from quantlib.time._businessdayconvention cimport BusinessDayConvention
+from quantlib.time.daycounter cimport DayCounter
 from quantlib.time._period cimport Days
 from quantlib.time._schedule cimport Rule
+from quantlib.termstructures.yield_term_structure cimport YieldTermStructure
+from quantlib.pricingengines.engine cimport PricingEngine
 cimport quantlib.indexes._ibor_index as _ii
 cimport quantlib.instruments._instrument as _in
 from quantlib.instruments.swap cimport VanillaSwap
@@ -64,10 +67,30 @@ cdef class MakeVanillaSwap:
         self._thisptr.withRule(rule)
         return self
 
+    def with_fixed_leg_tenor(self, Period t not None):
+        self._thisptr.withFixedLegTenor(deref(t._thisptr))
+        return self
+
+    def with_fixed_leg_day_count(self, DayCounter dc not None):
+        self._thisptr.withFixedLegDayCount(deref(dc._thisptr))
+        return self
+
     def with_floating_leg_tenor(self, Period t not None):
         self._thisptr.withFloatingLegTenor(deref(t._thisptr))
         return self
 
+    def with_floating_leg_day_count(self, DayCounter dc not None):
+        self._thisptr.withFloatingLegDayCount(deref(dc._thisptr))
+        return self
+
     def with_floating_leg_spread(self, Spread sp):
         self._thisptr.withFloatingLegSpread(sp)
+        return self
+
+    def with_discounting_term_structure(self, YieldTermStructure yts not None):
+        self._thisptr.withDiscountingTermStructure(yts._thisptr)
+        return self
+
+    def with_pricing_engine(self, PricingEngine engine not None):
+        self._thisptr.withPricingEngine(engine._thisptr)
         return self
