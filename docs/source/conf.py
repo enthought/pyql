3fc58fd7541a0a3728152c8b3e4f98181a2c3769
Guillaume Horel <guillaume.horel@gmail.com>
2022-01-19 15:08:15 -0500

    doc tweaks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary',
         'sphinx.ext.imgmath', 'sphinx.ext.napoleon']
-napoleon_use_param = False
+napoleon_use_param = True
 autosummary_generate = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -94,12 +94,23 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
 
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'relations.html',
+        'searchbox.html',
+        'donate.html',
+    ]
+}
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'font_size': 11
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
