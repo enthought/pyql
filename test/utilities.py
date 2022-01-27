fe17cc2d532a96b780e7eae0da8fe08f090c1746
Guillaume Horel <guillaume.horel@gmail.com>
2022-01-27 15:38:58 -0500

    add tests

--- test/utilities.py
+++ test/utilities.py
@@ -1,6 +1,9 @@
 from quantlib.time.api import Actual365Fixed, NullCalendar
 from quantlib.termstructures.yields.api import FlatForward
 
-def flat_rate(rate, dc=Actual365Fixed()):
-     return FlatForward(
-        settlement_days=0, calendar=NullCalendar(), forward=rate, daycounter=dc)
+def flat_rate(rate, dc=Actual365Fixed(), reference_date=None):
+     if reference_date is None:
+          return FlatForward(
+               settlement_days=0, calendar=NullCalendar(), forward=rate, daycounter=dc)
+     else:
+          return FlatForward(reference_date, rate, dc)
