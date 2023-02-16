ad8df4ff095df488824a6fb257e8cc85e08d1380
Guillaume Horel <guillaume.horel@gmail.com>
2023-02-16 15:43:09 -0500

    allow black_constant_vol without reference_date

--- quantlib/termstructures/volatility/equityfx/black_constant_vol.pyx
+++ quantlib/termstructures/volatility/equityfx/black_constant_vol.pyx
@@ -4,6 +4,7 @@ from quantlib.handle cimport shared_ptr
 from quantlib.time.calendar cimport Calendar
 from quantlib.time.date cimport Date
 from quantlib.time.daycounter cimport DayCounter
+from quantlib.types cimport Natural
 from .black_vol_term_structure cimport BlackVolatilityTermStructure
 from quantlib.quote cimport Quote
 from . cimport _black_vol_term_structure as _bvts
@@ -29,26 +30,49 @@ cdef class BlackConstantVol(BlackVolatilityTermStructure):
     """
 
     def __init__(self,
-                 Date reference_date not None,
+                 Date reference_date,
                  Calendar calendar not None,
                  volatility,
-                 DayCounter daycounter not None):
-
-        if isinstance(volatility, Quote):
-            self._thisptr.reset(
-                new _bcv.BlackConstantVol(
-                    deref(reference_date._thisptr),
-                    deref(calendar._thisptr),
-                    (<Quote>volatility).handle(),
-                    deref(daycounter._thisptr)
+                 DayCounter daycounter not None,
+                 settlement_days=None
+    ):
+        if reference_date is None and settlement_days is None:
+            raise ValueError("Exactly one of reference_date or settlement_days needs to be defined")
+        if reference_date is not None:
+            if isinstance(volatility, Quote):
+                self._thisptr.reset(
+                    new _bcv.BlackConstantVol(
+                        deref(reference_date._thisptr),
+                        deref(calendar._thisptr),
+                        (<Quote>volatility).handle(),
+                        deref(daycounter._thisptr)
+                    )
                 )
-            )
-        elif isinstance(volatility, float):
-            self._thisptr.reset(
-                new _bcv.BlackConstantVol(
-                    deref(reference_date._thisptr),
-                    deref(calendar._thisptr),
-                    <double>volatility,
-                    deref(daycounter._thisptr)
+            elif isinstance(volatility, float):
+                self._thisptr.reset(
+                    new _bcv.BlackConstantVol(
+                        deref(reference_date._thisptr),
+                        deref(calendar._thisptr),
+                        <double>volatility,
+                        deref(daycounter._thisptr)
+                    )
                 )
-            )
+        else:
+            if isinstance(volatility, Quote):
+                self._thisptr.reset(
+                    new _bcv.BlackConstantVol(
+                        <Natural>settlement_days,
+                        deref(calendar._thisptr),
+                        (<Quote>volatility).handle(),
+                        deref(daycounter._thisptr)
+                    )
+                )
+            elif isinstance(volatility, float):
+                self._thisptr.reset(
+                    new _bcv.BlackConstantVol(
+                        <Natural>settlement_days,
+                        deref(calendar._thisptr),
+                        <double>volatility,
+                        deref(daycounter._thisptr)
+                    )
+                )
