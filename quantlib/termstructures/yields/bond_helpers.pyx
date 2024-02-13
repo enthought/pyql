b30c9b6fabaeb498afb60712cbdb565359b49dc8
Guillaume Horel <guillaume.horel@gmail.com>
2024-02-13 13:59:50 -0500

    extend FixedRateBondHelper constructor

--- quantlib/termstructures/yields/bond_helpers.pyx
+++ quantlib/termstructures/yields/bond_helpers.pyx
@@ -1,6 +1,6 @@
-include '../../types.pxi'
-
+from quantlib.types cimport Natural, Real, Rate
 from cython.operator cimport dereference as deref
+from libcpp cimport bool
 from libcpp.vector cimport vector
 
 from quantlib.handle cimport shared_ptr, static_pointer_cast
@@ -11,10 +11,11 @@ from . cimport _bond_helpers as _bh
 from quantlib.instruments.bond cimport Bond, Type, Clean
 cimport quantlib.instruments._bond as _bond
 from quantlib.quote cimport Quote
-from quantlib.time.date cimport Date
+from quantlib.time.calendar cimport Calendar
+from quantlib.time.date cimport Date, Period
 from quantlib.time.schedule cimport Schedule
 from quantlib.time.daycounter cimport DayCounter
-from quantlib.time.businessdayconvention cimport BusinessDayConvention, Following
+from quantlib.time.businessdayconvention cimport BusinessDayConvention, Following, Unadjusted
 from quantlib.termstructures.yields.rate_helpers cimport RateHelper
 
 
@@ -37,9 +38,16 @@ cdef class FixedRateBondHelper(BondHelper):
                  Real face_amount, Schedule schedule not None,
                  vector[Rate] coupons,
                  DayCounter day_counter not None, BusinessDayConvention payment_conv=Following,
-                 Real redemption=100.0, Date issue_date=Date()):
+                 Real redemption=100.0,
+                 Date issue_date=Date(),
+                 Calendar payment_calendar=Calendar(),
+                 Period ex_coupon_period=Period(),
+                 Calendar ex_coupon_calendar=Calendar(),
+                 BusinessDayConvention ex_coupon_convention=Unadjusted,
+                 bool ex_coupon_end_of_month=False,
+                 Type price_type=Clean):
 
-        self._thisptr = shared_ptr[_bh.RateHelper](
+        self._thisptr.reset(
             new _bh.FixedRateBondHelper(
                 clean_price.handle(),
                 settlement_days,
@@ -49,5 +57,12 @@ cdef class FixedRateBondHelper(BondHelper):
                 deref(day_counter._thisptr),
                 payment_conv,
                 redemption,
-                deref(issue_date._thisptr)
-            ))
+                deref(issue_date._thisptr),
+                payment_calendar._thisptr,
+                deref(ex_coupon_period._thisptr),
+                ex_coupon_calendar._thisptr,
+                ex_coupon_convention,
+                ex_coupon_end_of_month,
+                price_type
+            )
+        )
