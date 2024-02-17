7246e3aab19006e114dda5ddde0ef569cb6c455d
Guillaume Horel <guillaume.horel@gmail.com>
2024-02-16 20:32:12 -0500

    closer to upstream constructor

--- quantlib/time/calendars/null_calendar.pyx
+++ quantlib/time/calendars/null_calendar.pyx
@@ -1,15 +1,12 @@
 cimport quantlib.time.calendars._null_calendar as _nc
-from quantlib.time.calendar cimport Calendar
-
 
 cdef class NullCalendar(Calendar):
     '''Calendar for reproducing theoretical calculations.
-    
-    This calendar has no holidays. It ensures that dates at whole-month 
+
+    This calendar has no holidays. It ensures that dates at whole-month
     distances have the same day of month.
 
     '''
 
     def __cinit__(self):
         self._thisptr = _nc.NullCalendar()
-
