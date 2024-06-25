8ba3fb220e47915b51afb8dbadeaa59cdd5440bb
Guillaume Horel <guillaume.horel@gmail.com>
2024-06-25 16:19:01 -0400

    bugfix

--- quantlib/termstructures/yields/forward_spreaded_term_structure.pyx
+++ quantlib/termstructures/yields/forward_spreaded_term_structure.pyx
@@ -1,6 +1,6 @@
 cimport quantlib.termstructures.yields._forward_spreaded_term_structure as _fsts
 
-from quantlib.termstructures.yield_term_structure cimport HandleYieldTermStructure
+from quantlib.termstructures.yield_term_structure cimport YieldTermStructure, HandleYieldTermStructure
 from quantlib.quote cimport Quote
 
 cdef class ForwardSpreadedTermStructure(YieldTermStructure):
