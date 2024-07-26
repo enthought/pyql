3f131a45e9f1844069de1cd79bb1cb18d3c20d9a
Guillaume Horel <guillaume.horel@gmail.com>
2024-07-26 14:18:24 -0400

    fix tests

--- test/test_inflation.py
+++ test/test_inflation.py
@@ -9,7 +9,7 @@
 
 import unittest
 
-from quantlib.indexes.inflation.australia import AUCPI
+from quantlib.indexes.inflation.aucpi import AUCPI
 from quantlib.time.date import Months, Period, Date
 from quantlib.time.api import ( UnitedKingdom, ModifiedFollowing, ActualActual,
                                 Schedule, Actual365Fixed, Unadjusted, Monthly )
@@ -31,7 +31,7 @@ from quantlib.cashflows.inflation_coupon_pricer import set_coupon_pricer
 class TestInflationIndex(unittest.TestCase):
 
     def test_zero_index(self):
-        aucpi = AUCPI(Monthly, True, True)
+        aucpi = AUCPI(Monthly, True)
         self.assertEqual(aucpi.name, "Australia CPI")
         self.assertEqual(aucpi.frequency, Monthly)
         self.assertEqual(aucpi.availability_lag, Period(2, Months))
@@ -51,7 +51,7 @@ class TestCPIBond(unittest.TestCase):
                                           ModifiedFollowing)
         self.cpi_ts = ZeroInflationTermStructure()
         self.yts = FlatForward(evaluation_date, 0.05, day_counter)
-        self.ii = UKRPI(False, self.cpi_ts)
+        self.ii = UKRPI(self.cpi_ts)
         fix_data = [206.1, 207.3, 208.0, 208.9, 209.7, 210.9,
                     209.8, 211.4, 212.1, 214.0, 215.1, 216.8,
                     216.5, 217.2, 218.4, 217.7, 216,
@@ -103,22 +103,22 @@ class TestCPIBond(unittest.TestCase):
             observation_lag,
             maturity, self.calendar, ModifiedFollowing, day_counter, self.ii, InterpolationType.AsIndex, self.yts) \
                         for maturity, r in zip(dates, rates)]
-        base_zero_rate = rates[0] / 100
+        base_date = self.ii.last_fixing_date
 
         self.cpi_ts.link_to(
             PiecewiseZeroInflationCurve(Interpolator.Linear,
-                                        evaluation_date, self.calendar, day_counter,
-                                        observation_lag, self.ii.frequency,
-                                        base_zero_rate, self.helpers))
+                                        evaluation_date, base_date, self.ii.frequency,
+                                        day_counter, self.helpers)
+        )
 
     def test_clean_price(self):
-        notional = 1000000.0;
+        notional = 1000000.0
         fixed_rates = [0.1]
         fixed_day_count = Actual365Fixed()
         fixed_calendar = self.calendar
         fixed_index = self.ii
-        contractObservationLag = Period(3, Months);
-        observationInterpolation = InterpolationType.Flat;
+        contractObservationLag = Period(3, Months)
+        observationInterpolation = InterpolationType.Flat
         settlement_days = 3
         growth_only = True
 
@@ -139,7 +139,7 @@ class TestCPIBond(unittest.TestCase):
         engine = DiscountingBondEngine(self.yts)
         cpi_bond.set_pricing_engine(engine)
         set_coupon_pricer(cpi_bond.cashflows, CPICouponPricer(self.yts))
-        storedPrice = 383.01816406
+        storedPrice = 383.04297558
         calculated = cpi_bond.clean_price
         self.assertAlmostEqual(storedPrice, calculated)
 
