04017513b48ceccfe4656bcd0f1f3848afcc9b31
Guillaume Horel <guillaume.horel@gmail.com>
2024-11-26 11:10:19 -0500

    JamshidianSwaptionEngine can take AffineModel directly

--- quantlib/pricingengines/swaption/jamshidian_swaption_engine.pyx
+++ quantlib/pricingengines/swaption/jamshidian_swaption_engine.pyx
@@ -5,8 +5,6 @@
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE.  See the license for more details.
 
-from cython.operator cimport dereference as deref
-
 from quantlib.handle cimport Handle, shared_ptr, static_pointer_cast
 cimport quantlib.pricingengines._pricing_engine as _pe
 cimport quantlib.models.shortrate._onefactor_model as _ofm
@@ -15,12 +13,12 @@ from quantlib.pricingengines.engine cimport PricingEngine
 from . cimport _jamshidian_swaption_engine as _jse
 
 from quantlib.termstructures.yield_term_structure cimport HandleYieldTermStructure
-from quantlib.models.shortrate.onefactormodels.hullwhite cimport HullWhite
+from quantlib.models.shortrate.onefactor_model cimport OneFactorAffineModel
 
 
 cdef class JamshidianSwaptionEngine(PricingEngine):
 
-    def __init__(self, HullWhite model not None,
+    def __init__(self, OneFactorAffineModel model not None,
                  HandleYieldTermStructure ts=HandleYieldTermStructure()):
 
         self._thisptr.reset(new _jse.JamshidianSwaptionEngine(
