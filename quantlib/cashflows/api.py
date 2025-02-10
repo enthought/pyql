5ecdb668741a4bac165b0b6522495b7839c521a9
Guillaume Horel <guillaume.horel@gmail.com>
2025-02-10 15:06:44 -0500

    add various legs to the api

--- quantlib/cashflows/api.py
+++ quantlib/cashflows/api.py
@@ -1,4 +1,5 @@
-from .overnight_indexed_coupon import OvernightIndexedCoupon
+from .overnight_indexed_coupon import OvernightIndexedCoupon, OvernightLeg
 from .cms_coupon import CmsCoupon
 from .ibor_coupon import IborCoupon
-from .fixed_rate_coupon import FixedRateCoupon
+from .fixed_rate_coupon import FixedRateCoupon, FixedRateLeg
+from ..cashflow import SimpleCashFlow, Leg
