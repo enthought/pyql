261a342c92a6f2b6416c6b6eff5abe85365202e4
Guillaume Horel <guillaume.horel@gmail.com>
2025-03-13 16:46:26 -0400

    cleanups

--- quantlib/pricingengines/vanilla/_mceuropeanhestonengine.pxd
+++ quantlib/pricingengines/vanilla/_mceuropeanhestonengine.pxd
@@ -1,16 +1,17 @@
-include '../../types.pxi'
-
+from quantlib.types cimport BigNatural, Real, Size
 from libcpp cimport bool
 from quantlib.processes._heston_process cimport HestonProcess
 from quantlib.pricingengines._pricing_engine cimport PricingEngine
 from quantlib.handle cimport shared_ptr
+from ._mcvanillaengine cimport MCVanillaEngine
+from quantlib.methods.montecarlo._mctraits cimport MultiVariate
 
 #needed to prevent a Forward declaration error
 cdef extern from 'ql/exercise.hpp':
     pass
 
-cdef extern from 'ql/pricingengines/vanilla/mceuropeanhestonengine.hpp' namespace 'QuantLib':
-    cdef cppclass MCEuropeanHestonEngine[RNG=*,S=*,P=*](PricingEngine):
+cdef extern from 'ql/pricingengines/vanilla/mceuropeanhestonengine.hpp' namespace 'QuantLib' nogil:
+    cdef cppclass MCEuropeanHestonEngine[RNG=*,S=*,P=*](MCVanillaEngine[MultiVariate, RNG, S]):
         MCEuropeanHestonEngine(shared_ptr[HestonProcess]& sp,
                                Size timeSteps,
                                Size timeStepsPerYear,
--- quantlib/pricingengines/vanilla/mceuropeanhestonengine.pyx
+++ quantlib/pricingengines/vanilla/mceuropeanhestonengine.pyx
@@ -1,18 +1,18 @@
 from quantlib.types cimport BigNatural, Integer, Real, Size
 from libcpp cimport bool
-from quantlib.handle cimport shared_ptr, static_pointer_cast
+from quantlib._defines cimport QL_MAX_INTEGER
+from quantlib.handle cimport static_pointer_cast
 from quantlib.utilities.null cimport Null
 from quantlib.processes.heston_process cimport HestonProcess
 cimport quantlib.processes._heston_process as _hp
-from .mcvanillaengine cimport MCVanillaEngine
 from . cimport _mceuropeanhestonengine as _mceh
 
 cdef class MCEuropeanHestonEngine(MCVanillaEngine):
 
-    def __init__(self, HestonProcess process, Size time_steps=Null[Integer](),
-                 Size steps_per_year=Null[Integer](), bool antithetic_variate=True,
+    def __init__(self, HestonProcess process, Size time_steps=Null[Size](),
+                 Size steps_per_year=Null[Size](), bool antithetic_variate=True,
                  Size required_samples=Null[Integer](), Real required_tolerance=Null[Real](),
-                 Size max_samples=Null[Integer](),
+                 Size max_samples=QL_MAX_INTEGER,
                  BigNatural seed=0):
         self._thisptr.reset(
             new _mceh.MCEuropeanHestonEngine(
