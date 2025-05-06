ac7dc42a894c553713c47cc27e50cda9208812a2
Guillaume Horel <guillaume.horel@gmail.com>
2025-05-06 15:36:08 -0400

    everything is unicode now

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -195,8 +195,8 @@ htmlhelp_basename = 'Quantlibcythonwrapperdoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'Quantlibcythonwrapper.tex', u'Quantlib cython wrapper Documentation',
-   u'Didrik Pinte \& Patrick Hénaff', 'manual'),
+  ('index', 'Quantlibcythonwrapper.tex', 'Quantlib cython wrapper Documentation',
+   'Didrik Pinte & Patrick Hénaff', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
