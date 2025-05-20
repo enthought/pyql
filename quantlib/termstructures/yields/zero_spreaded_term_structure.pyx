d95456f16038305d5086262b3eae7a1823912b83
Guillaume Horel <guillaume.horel@gmail.com>
2025-05-20 13:03:33 -0400

    bufgix

--- quantlib/termstructures/yields/zero_spreaded_term_structure.pyx
+++ quantlib/termstructures/yields/zero_spreaded_term_structure.pyx
@@ -1,5 +1,5 @@
 from cython.operator cimport dereference as deref
-from quantlib.compounding cimport Compounding, Continuous
+from quantlib.compounding cimport Compounding
 from quantlib.termstructures.yield_term_structure cimport HandleYieldTermStructure
 from quantlib.time.daycounter cimport DayCounter
 from quantlib.time.frequency cimport Frequency, NoFrequency
@@ -8,7 +8,7 @@ from . cimport _zero_spreaded_term_structure as _zsts
 
 cdef class ZeroSpreadedTermStructure(YieldTermStructure):
     def __init__(self, HandleYieldTermStructure h not None, Quote spread,
-                 Compounding comp=Continuous, Frequency freq=NoFrequency,
+                 Compounding comp=Compounding.Continuous, Frequency freq=NoFrequency,
                  DayCounter dc not None=DayCounter()):
 
         self._thisptr.reset(
